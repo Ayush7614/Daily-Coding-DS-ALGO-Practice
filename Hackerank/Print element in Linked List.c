a823899651b8ef4a3250a7f07a8c57caf4ae3f5b
ananya123-gif <65492788+ananya123-gif@users.noreply.github.com>
2021-05-09 22:34:03 +0530

    Create Print element in Linked List.c

---
+++ Hackerank/Print element in Linked List.c
@@ -0,0 +1,79 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+typedef struct SinglyLinkedListNode SinglyLinkedListNode;
+typedef struct SinglyLinkedList SinglyLinkedList;
+
+struct SinglyLinkedListNode {
+    int data;
+    SinglyLinkedListNode* next;
+};
+
+struct SinglyLinkedList {
+    SinglyLinkedListNode* head;
+    SinglyLinkedListNode* tail;
+};
+
+SinglyLinkedListNode* create_singly_linked_list_node(int node_data) {
+    SinglyLinkedListNode* node = malloc(sizeof(SinglyLinkedListNode));
+
+    node->data = node_data;
+    node->next = NULL;
+
+    return node;
+}
+
+void insert_node_into_singly_linked_list(SinglyLinkedList** singly_linked_list, int node_data) {
+    SinglyLinkedListNode* node = create_singly_linked_list_node(node_data);
+
+    if (!(*singly_linked_list)->head) {
+        (*singly_linked_list)->head = node;
+    } else {
+        (*singly_linked_list)->tail->next = node;
+    }
+
+    (*singly_linked_list)->tail = node;
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the printLinkedList function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+void printLinkedList(SinglyLinkedListNode* head) {
+struct SinglyLinkedListNode *p;
+    p = head;
+    while(p!=NULL)
+    {
+        printf("%d\n",p->data);
+        p=p->next;
+    }
+    printf("\n");
+
+}
+
+int main()
