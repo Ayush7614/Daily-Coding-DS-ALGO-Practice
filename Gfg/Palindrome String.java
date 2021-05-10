127a2906fa6cbe658220c0f7f075ec8fe379afb8
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-05-10 09:25:29 +0530

    Create  Palindrome String.java

---
+++ Gfg/Palindrome String.java
@@ -0,0 +1,43 @@
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+            
+            Solution ob = new Solution();
+            System.out.println(ob.isPlaindrome(S));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int isPlaindrome(String str) {
+       int i = 0, j = str.length() - 1; 
+  
+        // While there are characters toc compare 
+        while (i < j) { 
+  
+            // If there is a mismatch 
+            if (str.charAt(i) != str.charAt(j)) 
+                return 0; 
+  
+            // Increment first pointer and 
+            // decrement the other 
+            i++; 
+            j--; 
+        } 
+  
+        // Given string is a palindrome 
+        return 1; 
+    } 
+};
