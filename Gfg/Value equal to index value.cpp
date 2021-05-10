60235dea3cfb3e3e85191fa6a15a0ca8c185a2a0
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-05-10 09:30:56 +0530

    Create  Value equal to index value.cpp

---
+++ Gfg/Value equal to index value.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+
+	vector<int> valueEqualToIndex(int arr[], int n) {
+	    // code here
+	    vector<int> ans;
+	    for(int i=0;i<n;i++)
+	    {
+	        if(arr[i]==i+1)
+	        {
+	            ans.push_back(i+1);
+	        }
+	    }
+	    return ans;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.valueEqualToIndex(arr, n);
+        if (ans.empty()) {
+            cout << "Not Found";
+        } else {
+            for (int x: ans) {
+                cout << x << " ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
