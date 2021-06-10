13451016f6b9f4372d5e05d0b61173ac15dd14e9
Rajesh <rajrk2024@gmail.com>
2021-06-10 20:36:39 +0530

    Added solution of Fibonacci Modified in Python.

---
+++ Hackerank/Fibonacci Modified.py
@@ -0,0 +1,45 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the fibonacciModified function below.
+def fibonacciModified(t1, t2, n):
+    a = t1;
+    b = t2;
+    c = t1+ (t2*t2);
+    count = 3;
+    if (n == 1):
+        return t1;
+    elif (n == 2):
+        return t2;
+    elif(n == 3):
+        return c;
+    else:   
+        while(count < n):
+            count = count+ 1;
+            a = b;
+            b = c;
+            c = a + b*b;
+    return c;    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t1T2n = raw_input().split()
+
+    t1 = int(t1T2n[0])
+
+    t2 = int(t1T2n[1])
+
+    n = int(t1T2n[2])
+
+    result = fibonacciModified(t1, t2, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
