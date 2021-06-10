0109534d29fb25f52c271207843bad4510d216d5
div0510 <82434794+div0510@users.noreply.github.com>
2021-06-10 20:48:07 +0530

    Butterfly Pattern

---
+++ Butterfly.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Enter Number";
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if(j<=i)
+            cout<<"*";
+            else
+            cout<<" ";
+        }
+        for(int k=n;k>=1;k--)
+        {
+            if(k<=i)
+            cout<<"*";
+            else
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    for(int i=n;i>=1;i--)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if(j<=i)
+            cout<<"*";
+            else
+            cout<<" ";
+        }
+        for (int k = n; k >= 1; k--)
+        {
+            if(k<=i)
+            cout<<"*";
+            else
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
