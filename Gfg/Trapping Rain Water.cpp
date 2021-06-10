a5a5d895874c17a7a627157ec45a914c11c72db7
Prit Manvar <77919675+pritmanvar@users.noreply.github.com>
2021-06-10 20:54:54 +0530

    Update Trapping Rain Water.cpp

--- Gfg/Trapping Rain Water.cpp
+++ Gfg/Trapping Rain Water.cpp
@@ -1,4 +1,17 @@
-//Problem Link : https://practice.geeksforgeeks.org/problems/trapping-rain-water-1587115621/1
+// { Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+class Solution{
+
+    // Function to find the trapped water between the blocks.
+    public:
+    //Problem Link : https://practice.geeksforgeeks.org/problems/trapping-rain-water-1587115621/1
     int trappingWater(int arr[], int n){
         // Code here
         int water = 0;
@@ -23,3 +36,33 @@
         }
         return water;
     }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    
+    int t;
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        int n;
+        
+        //size of array
+        cin >> n;
+        
+        int a[n];
+        
+        //adding elements to the array
+        for(int i =0;i<n;i++){
+            cin >> a[i];            
+        }
+        Solution obj;
+        //calling trappingWater() function
+        cout << obj.trappingWater(a, n) << endl;
+        
+    }
+    
+    return 0;
+}  // } Driver Code Ends
