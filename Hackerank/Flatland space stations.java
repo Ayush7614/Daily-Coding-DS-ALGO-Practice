d74363e643f194302563d301fef4f7bdc7a16141
Rajesh <rajrk2024@gmail.com>
2021-06-11 20:01:42 +0530

    Added solution of Flatland space stations in Java.

---
+++ Hackerank/Flatland space stations.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the flatlandSpaceStations function below.
+    static int flatlandSpaceStations(int n, int[] c) {
+        int[] dist = new int[n];       
+        
+        for(int i = 0 ; i < n ; i++){
+            int min = Math.abs(c[0]-i);            
+            for(int j = 0 ; j < c.length ; j++){
+                int distance = Math.abs(c[j] - i);
+                if(min > distance){
+                    min = distance;
+                }               
+            }            
+            dist[i] = min;
+        }
+        int ans = dist[0];
+        for(int i = 0 ; i < n ; i++){
+            if(ans < dist[i]){
+                ans = dist[i];
+            }
+        }
+                
+        return ans;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0]);
+
+        int m = Integer.parseInt(nm[1]);
+
+        int[] c = new int[m];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int result = flatlandSpaceStations(n, c);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
