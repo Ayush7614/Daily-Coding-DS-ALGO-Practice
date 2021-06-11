b9387147a7c1ce90fd583f8133ff0c6e5e264fbb
Kaustav Roy <kaustavr25@gmail.com>
2021-06-12 01:00:23 +0530

    Max min in an array using built-in function

---
+++ Data Structures/max min in array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main(){
+    int size;
+    cout<<"Enter the size of array: ";
+    cin>>size;
+
+    int arr[size];
+    for(int i=0; i<size;i++){
+        cin>>arr[i];
+    }
+
+    int maxNo = INT_MIN, minNo=INT_MAX;
+
+    for(int i=0; i<size;i++){
+        maxNo = max(maxNo, arr[i]);
+        minNo = min(minNo, arr[i]);
+    }
+    cout<<"The max element is: "<<maxNo<<endl;
+    cout<<"The min element is: "<<minNo<<endl;
+    return 0;
+}
