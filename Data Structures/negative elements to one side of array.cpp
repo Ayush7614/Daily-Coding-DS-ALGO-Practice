63fa883bb9cf52c5d1729ddbab9e952a34e951fa
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-06-13 01:56:23 +0530

    Create negative elements to one side of array.cpp

---
+++ Data Structures/negative elements to one side of array.cpp
@@ -0,0 +1,40 @@
+/*
+An array contains both positive and negative numbers in random order. Rearrange the array elements so that all negative numbers appear before all positive numbers.
+Examples : 
+Input: -12, 11, -13, -5, 6, -7, 5, -3, -6
+Output: -12 -13 -5 -7 -3 -6 11 6 5
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cout.tie(nullptr);
+
+    int n; //123456  2
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    } 
+
+    int t = 0;
+    int i = -1;
+    for (int j = 0; j < n; j++)
+    {
+        if (arr[j] > t)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
