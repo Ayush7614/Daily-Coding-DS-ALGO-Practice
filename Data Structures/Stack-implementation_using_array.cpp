d65e5abff1f347899d6533713c40db581322921f
Drishika Chauhan <76254057+drishika2002@users.noreply.github.com>
2021-06-12 21:24:56 +0530

    Update Stack-implementation_using_array.cpp

--- Data Structures/Stack-implementation_using_array.cpp
+++ Data Structures/Stack-implementation_using_array.cpp
@@ -48,16 +48,29 @@ void display()
     }
 }
 
+int peek()
+{
+    if (top == -1)
+    {
+        cout << "Underflow" << endl;
+        return 0;
+    }
+    else
+    {
+        return stack[top];
+    }
+}
+
 int main()
-{ 
+{
     int ch;
     cout<<"Enter the number of elements: ";
     cin >> n;
     i=1;
     while(i)
-    { 
+    {
         cout<<"\nEnter your choice: "<<endl;
-        cout<<"1.PUSH\n2.POP\n3.DISPLAY\n4.EXIT\n";
+        cout<<"1.PUSH\n2.POP\n3.DISPLAY\n4.PEEK\n5.EXIT\n";
         cin >> ch;
         switch(ch)
         {
@@ -67,128 +80,81 @@ int main()
                 break;
             case 3: display();
                 break;
-            case 4: i = 0;
+            case 4: cout << peek();
+                break;
+            case 5: i = 0;
                 break;
             default:cout<<"Wrong Input... Enter again!"<<endl;
                 break;
         }
-    } 
-    return 0;  
+    }
+    return 0;
 }
 
 
 /*
-INPUT/OUTPUT 1:
-Enter the number of elements: 3
-
-Enter your choice: 
-1.PUSH
-2.POP
-3.DISPLAY
-4.EXIT
-2
-Stack underflow!
-
-Enter your choice: 
-1.PUSH
-2.POP
-3.DISPLAY
-4.EXIT
-1
-Enter element 1: 65
-
-Enter your choice: 
-1.PUSH
-2.POP
-3.DISPLAY
-4.EXIT
-1
-Enter element 1: 6
-
-Enter your choice: 
-1.PUSH
-2.POP
-3.DISPLAY
-4.EXIT
-2
-Value :6 at position 1 got deleted/popped out
-
-Enter your choice: 
-1.PUSH
-2.POP
-3.DISPLAY
-4.EXIT
-3
-Elements in the stack are :65 
-Enter your choice: 
-1.PUSH
-2.POP
-3.DISPLAY
-4.EXIT
-4
-
-INPUT/OUTPUT 2:
+INPUT/OUTPUT:
 Enter the number of elements: 5
 
-Enter your choice: 
+Enter your choice:
 1.PUSH
 2.POP
 3.DISPLAY
-4.EXIT
-1
-Enter element 1: 34
-
-Enter your choice: 
-1.PUSH
-2.POP
-3.DISPLAY
-4.EXIT
-1
-Enter element 1: 76
-
-Enter your choice: 
-1.PUSH
-2.POP
-3.DISPLAY
-4.EXIT
-1
-Enter element 1: -7
-
-Enter your choice: 
-1.PUSH
-2.POP
-3.DISPLAY
-4.EXIT
+4.PEEK
+5.EXIT
 1
 Enter element 1: 67
 
-Enter your choice: 
+Enter your choice:
 1.PUSH
 2.POP
 3.DISPLAY
-4.EXIT
+4.PEEK
+5.EXIT
 1
-Enter element 1: 23
+Enter element 1: 89
 
-Enter your choice: 
+Enter your choice:
 1.PUSH
 2.POP
 3.DISPLAY
-4.EXIT
-1
-Stack overflow!
-
-Enter your choice: 
-1.PUSH
-2.POP
-3.DISPLAY
-4.EXIT
-3
-Elements in the stack are :23 67 -7 76 34 
-Enter your choice: 
-1.PUSH
-2.POP
-3.DISPLAY
-4.EXIT
+4.PEEK
+5.EXIT
 4
+89
+
+Enter your choice:
+1.PUSH
+2.POP
+3.DISPLAY
+4.PEEK
+5.EXIT
+2
+Value :89 at position 1 got deleted/popped out
+
+Enter your choice:
+1.PUSH
+2.POP
+3.DISPLAY
+4.PEEK
+5.EXIT
+2
+Value :67 at position 0 got deleted/popped out
+
+Enter your choice:
+1.PUSH
+2.POP
+3.DISPLAY
+4.PEEK
+5.EXIT
+2
+Stack underflow!
+
+Enter your choice:
+1.PUSH
+2.POP
+3.DISPLAY
+4.PEEK
+5.EXIT
+5
 */
