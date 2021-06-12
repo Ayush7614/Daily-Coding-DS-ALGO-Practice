7c926b2c2ad3a419bb725443c03993f32de6bfbf
ARPIT GOYAL <arpitgoyal0927@gmail.com>
2021-06-12 11:34:49 +0530

    Hamming_distance added

---
+++ Leetcode/Hamming_Distance.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int z=x^y;
+        int sum=0;
+        while(z)
+        {
+            if(z&1) sum++;
+            z=z>>1;
+        }
+        return sum;
+        
+    }
+};
