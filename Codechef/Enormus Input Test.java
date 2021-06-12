74aec2ebdb9d6d3f0bcafb11281c0412f6dde343
Kishkin <joshi.kishkin10@gmail.com>
2021-06-12 12:46:34 +0530

    Added CodeChef Enormus Input Test in JAVA

---
+++ Codechef/Enormus Input Test.java
@@ -0,0 +1,60 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+
+// Remember that the class name should be "Main" and should be "public".
+public class Main {
+	public static void main(String[] args) {
+		// System.in and System.out are input and output streams, respectively.
+		InputStream inputStream = System.in;
+
+		InputReader in = new InputReader(inputStream);
+
+		int n = in.nextInt();
+		int k = in.nextInt();
+		
+		int ans = 0;
+		
+		for (int i = 0; i < n; i++) {
+			int x = in.nextInt();
+			
+			if (x % k == 0) {
+				ans++;
+			}
+		}
+
+		System.out.println(ans);
+	}
+
+	static class InputReader {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+
+		public InputReader(InputStream stream) {
+			reader = new BufferedReader(new InputStreamReader(stream), 32768);
+			tokenizer = null;
+		}
+
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+				    tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+				    throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+	}
+}
