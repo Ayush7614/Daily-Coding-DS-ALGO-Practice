c3b410a4fa16e7e9c8ec42147ecc6672ef6ec9f5
Tasnimur Rahaman Anas <anasfarazi0151@gmail.com>
2021-06-12 12:49:01 +0600

    C++ code for issue 435

---
+++ Codeforces/Phoenix and Puzzle.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+using namespace std;
+
+bool kmain(double a)
+{
+	ll p = sqrt(a);
+
+	if(p*p==a)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		ll n;
+		cin>>n;
+		if(n%2!=0)
+		{
+			cout<<"NO"<<endl;
+		}
+		else if(kmain(n/2) || (kmain(n/4) && n%4==0))
+		{
+			cout<<"YES"<<endl;
+		}
+		else
+		{
+			cout<<"NO"<<endl;
+		}
+	}
+	return 0;
+}
