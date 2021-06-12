0527b436be2d629756d53492870f3d379ae54852
nisha <nishayadav2548706@gmail.com>
2021-06-12 13:33:56 +0530

    watermelon

---
+++ Codeforces/watermelon.cpp
@@ -0,0 +1,19 @@
+//water melon problem cpp.
+//problem-4A
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cin>>n;
+    if(n%2==0 && n>2){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    return 0;
+    
+}
