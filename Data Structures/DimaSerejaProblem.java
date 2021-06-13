a55429c66135e21abbbda59f7e5c5599272b505c
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-06-13 16:31:56 +0530

    Added Dima-Sereja Problem in Java

--- Data Structures/DimaSerejaProblem.java
+++ Data Structures/DimaSerejaProblem.java
@@ -1,5 +1,7 @@
 /*
-Sereja and Dima play a game. The rules of the game are very simple. The players have n cards in a row. Each card contains a number, all numbers on the cards are distinct. The players take turns, Sereja moves first. During his turn a player can take one card: either the leftmost card in a row or the rightmost one. The game ends when there is no more cards. The player who has the maximum sum of numbers on his cards by the end of the game wins.
+Sereja and Dima play a game. The rules of the game are very simple. The players have n cards in a row. 
+Each card contains a number, all numbers on the cards are distinct. The players take turns, Sereja moves first. During his turn a player can take one card: either the leftmost card in a row or the rightmost one. 
+The game ends when there is no more cards. The player who has the maximum sum of numbers on his cards by the end of the game wins.
 Sereja and Dima are being greedy. Each of them chooses the card with the larger number during his move.Inna is a friend of Sereja and Dima. She knows which strategy the guys are using, so she wants to determine the final score, given the initial state of the game. Help her.
 
 Input:
