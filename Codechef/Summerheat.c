8ed965e9c77e8ca5c16aafa7c77236c62b7d8e0e
MADARAPU SATHWIKA <72800563+Sathwika-02@users.noreply.github.com>
2021-06-14 18:57:30 +0530

    Update Summerheat.c
    "Added code for Summer heat June long challenge-Div3-Codechef"

--- Codechef/Summerheat.c
+++ Codechef/Summerheat.c
@@ -2,8 +2,7 @@
 Problem statement: Given Chefland has 2 different types of coconut, type A and type B. Type A contains only xa milliliters of coconut water 
 and type B contains only xb grams of coconut pulp. Chef's nutritionist has advised him to consume Xa milliliters of coconut water and Xb grams 
 of coconut pulp every week in the summer. Find the total number of coconuts (type A + type B) that Chef should buy each week to keep himself active
-in the hot weather.
-*/
+in the hot weather. */
 //Solution:
 
 #include<stdio.h>
