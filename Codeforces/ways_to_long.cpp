1a12144fdf0925d559d84aa94feef7b81e4e5e9b
Nisha Yadav <76838660+nisha331@users.noreply.github.com>
2021-06-14 19:21:33 +0530

    A. Way Too Long Words codeforces

---
+++ Codeforces/ways_to_long.cpp
@@ -0,0 +1,25 @@
+//A. Way Too Long Words
+
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+   string s;
+   string sn;
+   cin>>s;
+   if(s.length()>10){
+      int count=s.length()-2;
+      cout<<s[0]<<count<<s[s.length()-1]<<endl;
+   }
+   else{
+       cout<<s<<endl;
+   }
+    }
+    return 0;
+    
+}
