f598f0b1b44242634fc791bdcf5340c14b349675
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-06-16 01:26:56 +0530

    Update Rabin Karp algorithm.cpp

--- Algorithms/Rabin Karp algorithm.cpp
+++ Algorithms/Rabin Karp algorithm.cpp
@@ -2,58 +2,61 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-
-#define d 10
-
-void rabinKarpAlgo(char patternInText[], char text[], int q) {
-  int m = strlen(patternInText);
-  int n = strlen(text);
-  int i, j;
-  int p = 0;
-  int t = 0;
-  int h = 1;
-
-  for (i = 0; i < m - 1; i++)
-    h = (h * d) % q;
-
-  // Calculating  hash value
-  for (i = 0; i < m; i++) {
-    p = (d * p + patternInText[i]) % q;
-    t = (d * t + text[i]) % q;
-  }
-
-  for (i = 0; i <= n - m; i++) {
-    if (p == t) {
-      for (j = 0; j < m; j++) {
-        if (text[i + j] != patternInText[j])
-          break;
+int main (){
+   char txt[80], pat[80];
+   int q;
+   cout<<"Enter the string to be searched : ";
+   cin>>txt;
+   cout<<"Enter the pattern : ";
+   cin>>pat;
+   int d = 256;
+   cout<<"Enter the prime number : ";
+   cin>>q;
+   int M = strlen (pat);
+   int N = strlen (txt);
+   int i, j;
+   int p = 0;
+   int t = 0;
+   int h = 1;
+   for (i = 0; i < M - 1; i++)
+      h = (h * d) % q;
+   for (i = 0; i < M; i++){
+      p = (d * p + pat[i]) % q;
+      t = (d * t + txt[i]) % q;
+   }
+   for (i = 0; i <= N - M; i++){
+      if (p == t){
+         for (j = 0; j < M; j++){
+            if (txt[i + j] != pat[j])
+            break;
+         }
+         if (j == M)
+            cout<<"Pattern found at index : "<<i<<endl;
       }
-
-      if (j == m)
-        cout << "Position of pattern :  " << i + 1 << endl;
-    }
-
-    if (i < n - m) {
-      t = (d * (t - text[i] * h) + text[i + m]) % q;
-
-      if (t < 0)
-        t = (t + q);
-    }
-  }
+      if (i < N - M){
+         t = (d * (t - txt[i] * h) + txt[i + M]) % q;
+         if (t < 0)
+            t = (t + q);
+      }
+   }
+   return 0;
 }
-
-int main() {
-  char text[] = "SUNDAY";
-  char patternInText[] = "DAY";
-  int q = 13;
-  rabinKarpAlgo(patternInText, text, q);
-}
-
-//Output : Position of pattern  : 4
 
 
 /*
+Input:
+Enter the string to be searched : SUNDAY
+Enter the pattern : DAY
+Enter the prime number : 13
+
+Output:
+Pattern found at index : 3
+
+
+
 The time complexity of the searching phase of the Karp-Rabin algorithm is O(mn) (when searching for am in an for instance).
 Its expected number of text character comparisons is O(n+m)
+
+*/
   
-  /*
+ 
