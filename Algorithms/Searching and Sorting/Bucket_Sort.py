4cfdbb0201e2fcb5d6b0304ca8a5cdcd484414df
ArshWalker <arshwalker7@gmail.com>
2021-06-16 17:31:55 +0530

    Bucket Sort added with I/O

--- Algorithms/Searching and Sorting/Bucket_Sort.py
+++ Algorithms/Searching and Sorting/Bucket_Sort.py
@@ -1,27 +1,44 @@
-def bucket_sort(input_list):
-    # Find maximum value in the list and use length of the list to determine which value in the list goes into which bucket 
-    max_value = max(input_list)
-    size = max_value/len(input_list)
+# using bucket sort
+def insertionSort(b):
+    for i in range(1, len(b)):
+        up = b[i]
+        j = i - 1
+        while j >= 0 and b[j] > up:
+            b[j + 1] = b[j]
+            j -= 1
+        b[j + 1] = up    
+    return b    
+             
+def bucketSort(x):
+    arr = []
+    slot_num = 10 # 10 means 10 slots, each
+                  # slot's size is 0.1
+    for i in range(slot_num):
+        arr.append([])
+         
+    # Put array elements in different buckets
+    for j in x:
+        index_b = int(slot_num * j)
+        arr[index_b].append(j)
+     
+    # Sort individual buckets
+    for i in range(slot_num):
+        arr[i] = insertionSort(arr[i])
+         
+    # concatenate the result
+    k = 0
+    for i in range(slot_num):
+        for j in range(len(arr[i])):
+            x[k] = arr[i][j]
+            k += 1
+    return x
+ 
+# Driver Code
+x = [0.897, 0.565, 0.656,
+     0.1234, 0.665, 0.3434]
+print("Sorted Array is")
+print(bucketSort(x))
 
-    # Create n empty buckets where n is equal to the length of the input list
-    buckets_list= []
-    for x in range(len(input_list)):
-        buckets_list.append([]) 
-
-    # Put list elements into different buckets based on the size
-    for i in range(len(input_list)):
-        j = int (input_list[i] / size)
-        if j != len (input_list):
-            buckets_list[j].append(input_list[i])
-        else:
-            buckets_list[len(input_list) - 1].append(input_list[i])
-
-    # Sort elements within the buckets using Insertion Sort
-    for z in range(len(input_list)):
-        insertion_sort(buckets_list[z])
-            
-    # Concatenate buckets with sorted elements into a single list
-    final_output = []
-    for x in range(len (input_list)):
-        final_output = final_output + buckets_list[x]
-    return final_output
+# Output will be: 
+# Sorted Array is
+# [0.1234, 0.3434, 0.565, 0.656, 0.665, 0.897]
