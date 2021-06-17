3056712fb7ba473f90ebd4de800f2a04f8a66afe
Adarsh357 <78082429+Adarsh357@users.noreply.github.com>
2021-06-17 20:26:57 +0530

    Updated hamming distance.cpp

--- Leetcode/Hamming_Distance.cpp
+++ Leetcode/Hamming_Distance.cpp
@@ -1,14 +1,18 @@
-class Solution {
-public:
-    int hammingDistance(int x, int y) {
-        int z=x^y;
-        int sum=0;
-        while(z)
-        {
-            if(z&1) sum++;
-            z=z>>1;
-        }
-        return sum; // returning the value
-        
-    }
-};
+#include <bits/stdc++.h>
+using namespace std;
+int hammingDistance(int n1, int n2)
+{
+    int x = n1 ^ n2;
+    int setBits = 0;
+    while (x > 0) {
+     setBits += x & 1;
+      x >>= 1;
+   }
+   return setBits;
+}
+int main()
+{
+  int n1 = 9, n2 = 14;
+  cout << hammingDistance(9, 14) << endl;
+    return 0;
+}
