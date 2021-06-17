04e9c52ef40331ef1b4852392299ab1e4f7d8fb3
Shivanshu Vishnoi <83551708+ShivanshuVishnoi@users.noreply.github.com>
2021-06-17 10:13:39 +0530

    Added Question and Test Cases in File

--- Codeforces/1343B_Balanced_Array.cpp
+++ Codeforces/1343B_Balanced_Array.cpp
@@ -1,6 +1,41 @@
+/*Question- You are given a positive integer n, it is guaranteed that n is even (i.e. divisible by 2).
+
+You want to construct the array a of length n such that:
+
+The first n2 elements of a are even (divisible by 2);
+the second n2 elements of a are odd (not divisible by 2);
+all elements of a are distinct and positive;
+the sum of the first half equals to the sum of the second half (∑i=1n2ai=∑i=n2+1nai).
+If there are multiple answers, you can print any. It is not guaranteed that the answer exists.
+
+You have to answer t independent test cases.
+
+
+INPUT
+5
+2
+4
+6
+8
+10
+
+OUTPUT
+NO
+YES
+2 4 1 5
+NO
+YES
+2 4 6 8 1 3 5 11
+NO
+
+*/
+
+
+
 #include <bits/stdc++.h>
 #define ll long int
 using namespace std;
+
 
 int main()
 {
