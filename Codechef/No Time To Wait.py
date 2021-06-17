128eaf256965dc58fe07875e564861d16434d140
jindal2209 <shubham7811@yahoo.com>
2021-06-17 22:00:16 +0530

    removed other files

--- Codechef/No Time To Wait.py
+++
@@ -1,8 +0,0 @@
-#Problem Link: https://www.codechef.com/MARCH21C/problems/NOTIME
-
-n, h, x = map(int, input().split())
-li = list(map(int, input().split()))
-if max(li)+x>=h:
-    print("YES")
-else:
-    print("NO")
