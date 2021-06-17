8d42d2e08506e004a254105c9d8e1ef5dc9dea6f
Vinayak Mohite <vinayakmohite1999@gmail.com>
2021-06-17 18:25:49 +0530

    Pratice -> Problem Solving

---
+++ Hackerank/Solve Me First.cpp
@@ -0,0 +1,14 @@
+// Problem Link: https://www.hackerrank.com/challenges/solve-me-first/problem
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);    
+    
+    int a, b;
+    cin >> a >> b;
+    cout << a+b << "\n";
+    return 0;
+}
