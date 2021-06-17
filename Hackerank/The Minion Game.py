cda880649c923517619135b80b861d3fdcfdf3c2
Adarsh357 <78082429+Adarsh357@users.noreply.github.com>
2021-06-17 19:59:45 +0530

    Updated the Minion Game.py

--- Hackerank/The Minion Game.py
+++ Hackerank/The Minion Game.py
@@ -1,17 +1,13 @@
 def minion_game(string):
-    # your code goes here
-    vowels_list = set(['a','e','i','o','u','A','E','I','O','U'])
-    consonants = 0
-    vowels = 0
-    n = len(string)
-    for i, l in enumerate(string):
-        if l in vowels_list:
-            vowels += n-i
+    scores = {"Kevin": 0, "Stuart": 0}
+    for i in range(len(string)):
+        if string[i] in "AEIOU":
+            scores["Kevin"]+=len(string)-i
         else:
-            consonants += n-i
-    if vowels == consonants:
-        print ("Draw")
-    elif vowels > consonants:
-        print ("Kevin {}".format(vowels))
+            scores["Stuart"]+=len(string)-i
+    if scores["Stuart"] == scores["Kevin"]:
+        print("Draw")
+    elif scores["Stuart"] > scores["Kevin"]:
+        print("%s %s" %("Stuart", scores["Stuart"]))
     else:
-        print ("Stuart {}".format(consonants))
+        print("%s %s" %("Kevin", scores["Kevin"]))
