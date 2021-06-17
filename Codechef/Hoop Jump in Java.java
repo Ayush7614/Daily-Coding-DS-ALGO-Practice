b9e1796f36a36a85149a14e0fb17e8c2f837c1cc
agamgupta2015 <75520558+agamgupta2015@users.noreply.github.com>
2021-06-17 21:49:43 +0530

    Create Hoop Jump in Java.java

---
+++ Codechef/Hoop Jump in Java.java
@@ -0,0 +1,22 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		long long int n;
+		Scanner sc = new Scanner(System.in);
+		n  = sc.next long long Int();
+		
+		while(n--)
+		{
+		    long long int m;
+		    m = sc.next long long Int();
+		    System.out.println(""+(m/2)+1);
+		}
+	}
+}
