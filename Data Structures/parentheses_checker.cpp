e30171b2021b0f422f81f1bba7ce23302c2a151d
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2021-06-17 16:28:31 +0530

    program file

---
+++ Data Structures/parentheses_checker.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+#include<stack>
+class Solution
+{
+    public:
+    //Function to check if brackets are balanced or not.
+    bool ispar(string s)
+    {
+        // Your code here
+        int n=s.length();
+        if(n==0)
+        return 0;
+        stack<char> v;
+    int c=0;
+    for(int i=0;s[i]!='\0';i++)
+    {
+        if((s[i]=='{') || (s[i]=='[') || (s[i]=='(')){
+        v.push(s[i]);
+        continue;}
+       
+        if (v.empty())
+            return false;
+           
+        else if((s[i]==')' && v.top()!='(') || (s[i]==']' && v.top()!='[') || (s[i]=='}' && v.top()!='{'))
+        {
+            c=1;
+            break;
+        }
+        else if((s[i]==')' && v.top()=='(') || (s[i]==']' && v.top()=='[') || (s[i]=='}' && v.top()=='{'))
+            v.pop();
+    }
+   
+    if(c==0 && v.empty()==1)
+    return true;
+    else
+    return false;
+
+    }
+   
+};
+int main()
+{
+    int t;
+    cin>>t;
+    string a;
+    while(t--)
+    {
+cin>>a;
+Solution obj;
+if(obj.ispar(a))
+  cout<<"balanced"<<endl;
+else
+  cout<<"unbalanced"<<endl;
+    }
+}     
