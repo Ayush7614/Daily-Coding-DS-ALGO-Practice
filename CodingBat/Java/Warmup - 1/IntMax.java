80bff33ec3316434c84a635b34e02174da73252f
Utkarsh <itsutkarsh123@gmail.com>
2021-06-18 18:05:05 +0530

    formatted intMax

--- CodingBat/Java/Warmup - 1/IntMax.java
+++ CodingBat/Java/Warmup - 1/IntMax.java
@@ -4,10 +4,8 @@
 // intMax(1, 3, 2) → 3
 // intMax(3, 2, 1) → 3
 
-public class IntMax 
-{
-    public int intMax(int a, int b, int c) 
-    {
+public class IntMax {
+    public int intMax(int a, int b, int c) {
         return Math.max(a, Math.max(b, c));
     }
 }
