21b30977161cf4615fdbc8c21f7966aff3b2c110
Trickaza <trickazaofficial@gmail.com>
2021-06-18 18:32:00 +0530

    Commit Done

--- Codechef/Chef and Groups.py
+++
@@ -1,8 +0,0 @@
-#Problem Link: https://www.codechef.com/MARCH21C/problems/GROUPS
-
-t = int(input())
-for i in range(t):
-    s = input()
-    s = '0'+s+'0'
-    c = s.count('01')
-    print(c)
--- Codechef/Possible Victory.py
+++
@@ -1,6 +0,0 @@
-#Problem Link: https://www.codechef.com/START2C/problems/T20MCH
-r, o, c = map(int, input().split())
-if c+36*(20-o)>r:
-    print("YES")
-else:
-    print("NO")
