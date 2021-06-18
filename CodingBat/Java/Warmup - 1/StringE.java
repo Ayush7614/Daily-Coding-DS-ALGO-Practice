b8982b4637dd4be2d9c2c7634ed9cf5175cfef92
Utkarsh <itsutkarsh123@gmail.com>
2021-06-18 18:13:08 +0530

    added stringE solution

---
+++ CodingBat/Java/Warmup - 1/StringE.java
@@ -0,0 +1,13 @@
+
+public class StringE {
+    public boolean stringE(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'e') {
+                count++;
+            }
+        }
+        return (count >= 1 && count <= 3);
+    }
+
+}
