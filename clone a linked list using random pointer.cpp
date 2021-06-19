890cec05801016a4837ef28dacbbd5b1c118430f
Jaya Rathore <64746051+jaya232@users.noreply.github.com>
2021-06-19 14:16:42 +0530

    clone a linked list with random pointer.cpp

---
+++ clone a linked list using random pointer.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+void print(Node *start)
+{
+    Node *ptr = start;
+    while (ptr)
+    {
+        cout << "[" << ptr->val << ","
+             << ptr->random->val <<"]"<<" ";
+        ptr = ptr->next;
+    }
+}
+class Solution {
+public:
+      Node* copyRandomList(Node* head) {
+        map<Node*, Node*> m;
+        int i=0;
+        Node* ptr = head;
+        while (ptr) {
+            m[ptr] =new Node(ptr->val);
+            ptr = ptr->next;
+        }
+        ptr = head;
+        while (ptr) {
+            m[ptr]->next = m[ptr->next];
+            m[ptr]->random = m[ptr->random];
+            ptr = ptr->next;
+        }
+        return m[head];
+    }
+};
+int main()
+{
+    Node* start = new Node(1);
+    start->next = new Node(2);
+    start->next->next = new Node(3);
+    start->next->next->next = new Node(4);
+    start->next->next->next->next = new Node(5);
+ 
+    // 1's random points to 3
+    start->random = start->next->next;
+ 
+    // 2's random points to 1
+    start->next->random = start;
+ 
+    // 3's and 4's random points to 5
+    start->next->next->random =
+                    start->next->next->next->next;
+    start->next->next->next->random =
+                    start->next->next->next->next;
+ 
+    // 5's random points to 2
+    start->next->next->next->next->random =
+                                      start->next;
+
+    cout << "Original list : \n";
+    print(start);
+
+    Solution obj;
+
+    cout << "\nCloned list : \n";
+    Node *cloned_list = obj.copyRandomList(start);
+    print(cloned_list);
+    return 0;
+}
