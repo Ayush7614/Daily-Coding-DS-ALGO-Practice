529ae08308052c5a3973e60d7a775ab6256cc95d
Sakshi Ankleshwariya <62878182+19bce502@users.noreply.github.com>
2021-06-21 00:23:18 +0530

    Update Longest Consecutive Sequence.cpp

--- Algorithms/Longest Consecutive Sequence.cpp
+++ Algorithms/Longest Consecutive Sequence.cpp
@@ -1,3 +1,5 @@
+//Problem Link 
+//https://leetcode.com/problems/longest-consecutive-sequence/
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
@@ -29,3 +31,13 @@ public:
          return maxAns;
     }
 };
+//testcases
+// Example 1:
+
+// Input: nums = [100,4,200,1,3,2]
+// Output: 4
+// Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+// Example 2:
+
+// Input: nums = [0,3,7,2,5,8,4,6,0,1]
+// Output: 9
