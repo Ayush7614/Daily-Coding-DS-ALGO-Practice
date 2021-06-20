2198d70208cfcb8e00bbe76185147c40347b65f3
Vinayak Mohite <vinayakmohite1999@gmail.com>
2021-06-20 16:35:59 +0530

    Practice -> Problem Solving

---
+++ Hackerank/Jumping on the Clouds: Revisited.cpp
@@ -0,0 +1,85 @@
+// Problem Link: https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> split_string(string);
+
+int jumpingOnClouds(vector<int> c, int k) {
+    int e=100, i=0;
+    i=(i+k)%c.size();
+    while(true) {
+        if(c[i]==0)
+            e = e-1;
+        else
+            e = e-3;
+        if (i==0)
+            break;
+        i=(i+k)%c.size();
+    }
+    return e;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nk_temp;
+    getline(cin, nk_temp);
+
+    vector<string> nk = split_string(nk_temp);
+
+    int n = stoi(nk[0]);
+
+    int k = stoi(nk[1]);
+
+    string c_temp_temp;
+    getline(cin, c_temp_temp);
+
+    vector<string> c_temp = split_string(c_temp_temp);
+
+    vector<int> c(n);
+
+    for (int i = 0; i < n; i++) {
+        int c_item = stoi(c_temp[i]);
+
+        c[i] = c_item;
+    }
+
+    int result = jumpingOnClouds(c, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
