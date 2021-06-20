157e719eacd4f3e0ff2bf4d896ec36e5a094da83
agamgupta2015 <75520558+agamgupta2015@users.noreply.github.com>
2021-06-20 14:03:57 +0530

    Create First Missing Positive.cpp

---
+++ Leetcode/First Missing Positive.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int firstMissingPositive(vector<int>& nums)
+{
+   int n = nums.size();
+   int j = 0;
+      for(int i=0; i<n; i++)
+       {
+         int curr = nums[i];
+           while(curr>=1 && curr<=n && nums[curr-1]! = curr)
+            {
+             int temp = nums[curr-1];
+                nums[curr-1] = curr;
+                curr = temp;
+            }
+       }
+       for(int i=0; i<n; i++)
+        {
+           if(nums[i]! = i+1) 
+             return i+1; 
+        }
+        return n+1;
+}
+    
+int main()
+{
+  vector <int>a ={3,4,-1,1};
+  cout<<firstMissingPositive(a)<<endl;
+  return 0;
+}
