6158697c9d697c73d8c742c00dbf4e7e739a055d
Vinayak Mohite <vinayakmohite1999@gmail.com>
2021-06-21 09:12:46 +0530

    Practice -> Problem Solving

---
+++ Hackerank/Sherlock and Squares.cpp
@@ -0,0 +1,70 @@
+// Problem Link: https://www.hackerrank.com/challenges/sherlock-and-squares/problem
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> split_string(string);
+
+int squares(int a, int b) {
+    if(sqrt(a)-floor(sqrt(a)) == 0)
+        return floor(sqrt(b))-floor(sqrt(a))+1;
+    else
+        return floor(sqrt(b))-floor(sqrt(a));
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string ab_temp;
+        getline(cin, ab_temp);
+
+        vector<string> ab = split_string(ab_temp);
+
+        int a = stoi(ab[0]);
+
+        int b = stoi(ab[1]);
+
+        int result = squares(a, b);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
