9cfeb4a39b8e4304c9497d92223e7b80b323daff
Ashu-1309 <ashutoshkumarsaw1309@gmail.com>
2021-06-21 19:54:35 +0530

    Longest Consecutive Sequence using Python

---
+++ Algorithms/Longest_Consecutive_Sequence.py
@@ -0,0 +1,15 @@
+class Solution(object): #main function
+   def longestConsecutive(self, values): #sub funcction
+      longest_consecutive_series = 0 # Initializing
+      for i in values:  # Iteration the given value
+         if i-1 not in values:  # condition check
+            value = i
+            count_of_consecutive_numbers = 0
+            while i in values:
+               i+=1
+               count_of_consecutive_numbers+=1
+               longest_consecutive_series = max(longest_consecutive_series,count_of_consecutive_numbers) #maximum finding
+      return longest_consecutive_series
+).longestConsecutive({13,15,19,16,21,17,18,23,1,4})) #Calling two function Solution and longestConsecutive
+
+print(Solution(
