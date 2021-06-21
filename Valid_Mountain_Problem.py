f22798e0d5da8d7b0c39a301e4fcc2394131ebd5
apran-vs <77320691+apran-vs@users.noreply.github.com>
2021-06-21 16:58:46 +0530

    Update Valid_Mountain_Problem.py

--- Valid_Mountain_Problem.py
+++ Valid_Mountain_Problem.py
@@ -2,23 +2,18 @@ def Valid_Mount(arr:list[int]):
     if len(arr) < 3 :
         return False
     else :
-        result = False
         for i in range(0, len(arr)-1 ):
             if arr[i+1] > arr[i] :
-                # result = True
                 continue
             else :
                 break
 
         for j in range(i , len(arr)-1 ) :
             if arr[j+1] < arr[j] :
-                # result = True
                 continue
             else :
-                # result = False
                 return False
 
-        # return result
         return True
 
 
