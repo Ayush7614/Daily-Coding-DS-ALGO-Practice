fc53e7b8e3b149a02f82cddd716c899e9c5e7b63
Vinayak Mohite <vinayakmohite1999@gmail.com>
2021-06-22 09:58:09 +0530

    Create Equalize the Array

---
+++ Hackerank/Equalize the Array.cpp
@@ -0,0 +1,76 @@
+// Problem Link: https://www.hackerrank.com/challenges/equality-in-a-array/problem
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> split_string(string);
+
+int equalizeArray(vector<int> arr) {
+    vector<int>occurrence(101);
+    for (int i=0; i<101; i++)
+        occurrence[i]=0;
+    for (int i=0; i<arr.size(); i++)
+        occurrence[arr[i]]++;
+    int maxx=0;
+    for (int i=0; i<101; i++)
+        maxx = max(maxx, occurrence[i]);
+    return arr.size()-maxx;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = equalizeArray(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
