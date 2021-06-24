1a6ea55b1034a44335af58e8ca7d9d5d17e807c5
Manu Varghese <manuvarghese0603@gmail.com>
2021-06-24 21:55:59 +0530

    Added sample input/output

--- Codechef/Chefland Visa (VISA).py
+++ Codechef/Chefland Visa (VISA).py
@@ -6,3 +6,18 @@ for i in range(t):
         print("YES")
     else:
         print("NO")
+
+# Sample input/output
+
+# Example Input
+# 4
+# 20 50 2100 1900 6 6
+# 50 20 1900 1900 5 5
+# 20 20 1900 1900 1 6
+# 27 27 1920 1920 3 3
+
+# Example Output
+# NO
+# NO
+# NO
+# YES
