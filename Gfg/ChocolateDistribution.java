ba2fc857b39108cc1b3e23eec8101eecede6c050
mdinesh124 <79132253+mdinesh124@users.noreply.github.com>
2021-06-24 22:30:10 +0530

    Update ChocolateDistribution.java

--- Gfg/ChocolateDistribution.java
+++ Gfg/ChocolateDistribution.java
@@ -1,3 +1,16 @@
+/*Test Case 1:
+Input : n = 5, a[] = {3, 2, 7, 6, 1} , m = 3
+Output: Minimum difference is 2 
+
+Test Case 2:
+Input : n = 7, a[] = {8, 5, 6, 3, 2, 7, 1} , m = 5
+Output: Minimum difference is 5
+
+
+*/
+
+
+
 import java.util.*;
 class ChocolateDistribution {
     static int Min( int n,int a[],int m)
