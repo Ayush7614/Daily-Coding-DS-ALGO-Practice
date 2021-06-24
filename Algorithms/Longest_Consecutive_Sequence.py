60dac2afc397ec327acf6391c6b256a9686fc2c0
Ashutosh Kumar Saw <60690692+Ashu-1309@users.noreply.github.com>
2021-06-24 20:15:46 +0530

    Adding comment and shorting the variable name
    Did some changes that are required to

--- Algorithms/Longest_Consecutive_Sequence.py
+++ Algorithms/Longest_Consecutive_Sequence.py
@@ -1,13 +1,16 @@
+
+# lcs = longest_consecutive_series
+# ccn = count_of_consecutive_numbers
 class Solution(object): #main function
    def longestConsecutive(self, values): #sub funcction
-      longest_consecutive_series = 0 # Initializing
+      lcs = 0 # Initializing
       for i in values:  # Iteration the given value
          if i-1 not in values:  # condition check
             value = i
-            count_of_consecutive_numbers = 0
+            ccn = 0
             while i in values:
-               i+=1
-               count_of_consecutive_numbers+=1
-               longest_consecutive_series = max(longest_consecutive_series,count_of_consecutive_numbers) #maximum finding
-      return longest_consecutive_series
+               i+=1  #increament
+               ccn+=1  #increment
+               lcs = max(lcs,ccn) #maximum finding
+      return lcs
 print(Solution().longestConsecutive({13,15,19,16,21,17,18,23,1,4})) #Calling two function Solution and longestConsecutive
