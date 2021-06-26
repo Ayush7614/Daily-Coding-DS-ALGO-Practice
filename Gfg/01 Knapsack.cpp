b325f1a620633d7842376b65aa2e1427aedf2a7e
Rounak-chauhan <77567098+Rounak-chauhan@users.noreply.github.com>
2021-06-27 00:09:05 +0530

    Update 01 Knapsack.cpp
    instead of recursion i prefer using dp approch for this question which reduces the time complexity and same value is not returning again

--- Gfg/01 Knapsack.cpp
+++ Gfg/01 Knapsack.cpp
@@ -10,34 +10,25 @@ int max(int a, int b) { return (a > b) ? a : b; }
 
 
 
-int knapSack(int W, int wt[], int val[], int n)
+int knapSack(int maxweight,int[] weight,int[] value,int n)
 {
-
-
-
-	// Base Case
-
-	if (n == 0 || W == 0)
+	int storage[n+1][maxweight+1];
+	for(int i=0;i<=n;i++)
+	{
+		for(int j=0;j<=maxweight;j++)
+		{
+			//base case
+			if (i==0 || j==0) 
+                             storage[i][w] = 0;
+			else if(weight[i-1]>maxweight)
+		               storage[i][j]=storage[i-1][j];
+			else
+				storage[i][j]=max(storage[i-1][j],storage[i-1][j-weight[i-1]]+value[i-1];
+		}
+	}
 	
-	return 0;
-
-
-	
-
-        if (wt[n - 1] > W)
-	
-	return knapSack(W, wt, val, n - 1);
-
-
-	
-        
-        else
-	
-	return max(
-val[n - 1]
-+ knapSack(W - wt[n - 1],
-wt, val, n - 1),
-knapSack(W, wt, val, n - 1));
+	return storage[n][maxweight];					  
+						  
 
 }
 
