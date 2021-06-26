4bf52a19e63fbae4708a77b8e1bd6c9da1e342f5
Manu Varghese <manuvarghese0603@gmail.com>
2021-06-26 17:37:57 +0530

    Modified the code

--- Codechef/XOR Folding (XOREQUAL).cpp
+++ Codechef/XOR Folding (XOREQUAL).cpp
@@ -1,14 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-long long int mpow(long long int);
+#define ll long long
 int main()
 {
-    long long int t,n,b;
+    ll int t;
     cin>>t;
     while(t--)
     {
-        cin>>n;
-        b=mpow(n);
-        cout<<b/2<<endl;
+        ll int n,m,ones=0;
+        cin>>n>>m;
+        string s[n];
+        for(int i=0;i<n;i++)
+        cin>>s[i];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<s[i].size();j++)
+            if(s[i][j]=='1')
+            ones++;
+        }
+        if(ones%2!=0)
+        cout<<"YES"<<endl;
+        else 
+        cout<<"NO"<<endl;
     }
 }
