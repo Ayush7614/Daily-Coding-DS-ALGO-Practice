5db0765689c8d4fc2e7281ea35c4fa387b0b4435
pranjay-poddar <pranjaypoddar@outlook.com>
2021-06-27 20:06:18 +0530

    Added Node_Marking - Codechef June lunchtime21

---
+++ Codechef/Node_Marking.cpp
@@ -0,0 +1,171 @@
+
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include<iostream>
+#include<string>
+#include<cstdio>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<iomanip>
+#include<queue>
+#include<ciso646>
+#include<random>
+#include<map>
+#include<set>
+#include<bitset>
+#include<stack>
+#include<unordered_map>
+#include<unordered_set>
+#include<utility>
+#include<cassert>
+#include<complex>
+#include<numeric>
+#include<array>
+using namespace std;
+
+//#define int long long
+typedef long long ll;
+
+typedef unsigned long long ul;
+typedef unsigned int ui;
+constexpr ll mod = 1000000007;
+const ll INF = mod * mod;
+typedef pair<int, int>P;
+#define stop char nyaa;cin>>nyaa;
+#define rep(i,n) for(int i=0;i<n;i++)
+#define per(i,n) for(int i=n-1;i>=0;i--)
+#define Rep(i,sta,n) for(int i=sta;i<n;i++)
+#define rep1(i,n) for(int i=1;i<=n;i++)
+#define per1(i,n) for(int i=n;i>=1;i--)
+#define Rep1(i,sta,n) for(int i=sta;i<=n;i++)
+#define all(v) (v).begin(),(v).end()
+typedef pair<ll, ll> LP;
+typedef double ld;
+typedef pair<ld, ld> LDP;
+const ld eps = 1e-8;
+const ld pi = acosl(-1.0);
+
+ll mod_pow(ll x, ll n, ll m = mod) {
+	if (n < 0) {
+		ll res = mod_pow(x, -n, m);
+		return mod_pow(res, m - 2, m);
+	}
+	if (abs(x) >= m)x %= m;
+	if (x < 0)x += m;
+	ll res = 1;
+	while (n) {
+		if (n & 1)res = res * x % m;
+		x = x * x % m; n >>= 1;
+	}
+	return res;
+}
+struct modint {
+	ll n;
+	modint() :n(0) { ; }
+	modint(ll m) :n(m) {
+		if (n >= mod)n %= mod;
+		else if (n < 0)n = (n % mod + mod) % mod;
+	}
+	operator int() { return n; }
+};
+bool operator==(modint a, modint b) { return a.n == b.n; }
+modint operator+=(modint& a, modint b) { a.n += b.n; if (a.n >= mod)a.n -= mod; return a; }
+modint operator-=(modint& a, modint b) { a.n -= b.n; if (a.n < 0)a.n += mod; return a; }
+modint operator*=(modint& a, modint b) { a.n = ((ll)a.n * b.n) % mod; return a; }
+modint operator+(modint a, modint b) { return a += b; }
+modint operator-(modint a, modint b) { return a -= b; }
+modint operator*(modint a, modint b) { return a *= b; }
+modint operator^(modint a, ll n) {
+	if (n == 0)return modint(1);
+	modint res = (a * a) ^ (n / 2);
+	if (n % 2)res = res * a;
+	return res;
+}
+
+ll inv(ll a, ll p) {
+	return (a == 1 ? 1 : (1 - p * inv(p % a, a)) / a + p);
+}
+modint operator/(modint a, modint b) { return a * modint(inv(b, mod)); }
+modint operator/=(modint& a, modint b) { a = a / b; return a; }
+const int max_n = 1 << 20;
+modint fact[max_n], factinv[max_n];
+void init_f() {
+	fact[0] = modint(1);
+	for (int i = 0; i < max_n - 1; i++) {
+		fact[i + 1] = fact[i] * modint(i + 1);
+	}
+	factinv[max_n - 1] = modint(1) / fact[max_n - 1];
+	for (int i = max_n - 2; i >= 0; i--) {
+		factinv[i] = factinv[i + 1] * modint(i + 1);
+	}
+}
+modint comb(int a, int b) {
+	if (a < 0 || b < 0 || a < b)return 0;
+	return fact[a] * factinv[b] * factinv[a - b];
+}
+modint combP(int a, int b) {
+	if (a < 0 || b < 0 || a < b)return 0;
+	return fact[a] * factinv[a - b];
+}
+
+void solve() {
+	int n, k; cin >> n >> k;
+	vector<int> p(n);
+	vector<vector<int>> G(n);
+	rep1(i, n - 1) {
+		cin >> p[i]; p[i]--;
+		G[p[i]].push_back(i);
+	}
+	vector<int> sz(n);
+	function<void(int)> dfs = [&](int id) {
+		sz[id] = 1;
+		for (int to : G[id]) {
+			dfs(to);
+			sz[id] += sz[to];
+		}
+	};
+	dfs(0);
+	vector<int> dp(k + 1,mod);
+	dp[0] = 0;
+
+	vector<vector<int>> ad(n, vector<int>(k+1,mod));
+	function<void(int)> dpdfs = [&](int id) {
+		rep(i, k + 1)if (dp[i] < mod) {
+			if (i + sz[id] <= k) {
+				ad[id][i + sz[id]] = dp[i] + 1;
+			}
+		}
+		for (int to : G[id]) {
+			dpdfs(to);
+		}
+		rep(i, k + 1) {
+			dp[i] = min(dp[i], ad[id][i]);
+		}
+	};
+	dpdfs(0);
+	vector<int> sdp(k + 1, mod);
+	sdp[0] = -1;
+	rep(i, k) {
+		rep1(j, k) {
+			if (i + j <= k) {
+				sdp[i + j] = min(sdp[i + j], sdp[i] + 1 + dp[j]);
+			}
+		}
+	}
+	cout << sdp[k] << "\n";
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	//cout << fixed << setprecision(10);
+	//init_f();
+	//init();
+	//expr();
+	int t; cin >> t; rep(i, t)
+	solve();
+	return 0;
+}
+
