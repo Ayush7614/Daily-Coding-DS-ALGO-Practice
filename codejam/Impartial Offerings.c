5bce76f90f0a1e26357c2465c7c1ece9c36e4cbd
Sakina Nomi <sakinanomi2011@gmail.com>
2021-06-27 10:39:43 +0530

    Solution of problem #include<stdio.h>

---
+++ codejam/Impartial Offerings.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void main()
+{
+    int t;//number of test cases
+    scanf("%d",&t);
+
+    int temp2=1;//tracking the testcase
+    while(t--)
+    {
+        int n;//number of pets
+        scanf("%d",&n);
+        int temp;
+        int sz[100]={0};//this array tracks the number of pets with each size
+        for(int i=0;i<n;i++)
+        {
+            scanf("%d",&temp);
+            sz[temp-1]=sz[temp-1]+1;//incrementing the value of index according to size found
+        }
+        long ans=0;//this will store our answer
+        long incre=1;//this indicates number of treats for the pet of particular size
+
+        for(int i=0;i<100;i++)
+        {
+	    /*The first pet size found with non-zero will be smallest among the pets
+	      hence gets 1 and accordingly the number increases*/
+            if(sz[i]!=0)
+            {
+                ans=ans+sz[i]*incre;
+                incre++;
+            }
+        }
+        printf("Case #%d: %ld\n",temp2,ans);//printing the output
+        temp2++;
+    }
+}
