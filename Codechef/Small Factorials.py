b0b1502713d24c8e797d9f8faee01f7a1fc40574
Rajesh <rajrk2024@gmail.com>
2021-06-28 11:43:51 +0530

    Added Example testcase for Small Factorials in python.

--- Codechef/Small Factorials.py
+++ Codechef/Small Factorials.py
@@ -9,3 +9,18 @@ test = int(input())
 for i in range (0,test):
     val = int(input())
     print(fact(val))
+
+
+##Example Test Case
+# Sample input:
+# 4
+# 1
+# 2
+# 5
+# 3
+# Sample output:
+#
+# 1
+# 2
+# 120
+# 6
