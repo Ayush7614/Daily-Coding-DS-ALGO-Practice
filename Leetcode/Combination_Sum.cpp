d0756c2393381a9780a7d39fba3c4b9e34712df4
Harshpreet Kaur <harshpreet0508@gmail.com>
2021-06-29 10:19:16 +0530

    Update Combination_Sum.cpp

--- Leetcode/Combination_Sum.cpp
+++ Leetcode/Combination_Sum.cpp
@@ -22,6 +22,7 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         
         sort(candidates.begin(),candidates.end());    // sort 
+        candidates.erase(unique(candidates.begin(), candidates.end()), candidates.end());
         vector<vector<int>>res;                       // resultant vector
         vector<int>combination;                       // trying combinations
         
@@ -30,4 +31,36 @@ public:
         
     }
 };
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> A;
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            A.push_back(x);
+        }   
+        int sum;
+        cin>>sum;
+        Solution ob;
+        vector<vector<int>> result = ob.combinationSum(A, sum);
+   		if(result.size()==0){
+   			cout<<"Empty";
+   		}
+        for(int i=0;i<result.size();i++){
+            cout<<'(';
+            for(int j=0;j<result[i].size();j++){
+                cout<<result[i][j];
+                if(j<result[i].size()-1)
+                    cout<<" ";
+            }
+            cout<<")";
+        }
+        cout<<"\n";
+    }
+}
 
