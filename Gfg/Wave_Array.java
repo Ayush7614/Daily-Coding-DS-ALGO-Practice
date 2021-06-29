f360c8c3981843434de7ae4be62b623d17cf3af7
Aryan <aryanyash999@gmail.com>
2021-06-29 21:36:33 +0530

    Added Wave Array

---
+++ Gfg/Wave_Array.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class SortWave
+{
+    // method return the wave array
+    public void sortInWave(int arr[], int n){
+        
+        for(int i =1 ;i < n; i+=2){
+            swap(arr,i);
+        }
+        
+    }
+    //swap alternate elements of array.
+    public void swap(int arr[], int i){
+        int temp = arr[i-1];
+        arr[i-1] = arr[i];
+        arr[i] = temp;
+    }
+
+    public static void main(String args[])
+    {
+        SortWave ob = new SortWave();
+        Scanner sc = new Scanner(System.in);
+        //Inputting Testcases
+        int t = sc.nextInt();
+        while(t-- > 0){
+            //NOTE : In this question we've sorted array.
+            //If the array is not sorted first sort the array, you can use Arrays.sort() function.
+
+            //Input length of array
+            int n = sc.nextInt();
+            //array of length n.
+            int arr[] = new int[n];
+            //Input Array
+            for(int i = 0 ; i < n; i++)
+                arr[i] = sc.nextInt();
+            
+            ob.sortInWave(arr, n);
+
+            for (int i : arr)
+                System.out.print(i + " ");
+        }
+        sc.close();
+    }
+}
+//Time Complexity : O(N)
+//Space Complexity : O(1)
