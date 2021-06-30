15e96046506ab5895f795ec9c76884f0fa0c9f61
Manu Varghese <manuvarghese0603@gmail.com>
2021-06-30 12:45:38 +0530

    Added intro and explanation

--- Codechef/Chess Match (BLITZ3_2).cpp
+++ Codechef/Chess Match (BLITZ3_2).cpp
@@ -1,18 +1,24 @@
+// Intro:
+// Problem link: https://www.codechef.com/problems/BLITZ3_2
+// The total time remaining on each players clock after the game is concluded is given by a and b, therefore the total time remaining is (a+b)
+// The total time provided to each player is (180 + N) seconds therefore total time provided is 2*(180 + N)
+// The duration of the game = total time provided - Total time remaining
+
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
 void solve()
 {
     int n,a,b;
-    cin>>n>>a>>b;
-    cout<<(2*(180+n)-(a+b))<<"\n";
+    cin>>n>>a>>b;                                   
+    cout<<(2*(180+n)-(a+b))<<"\n";                     // output (total time provided - Total time remaining)
 }
 int main()
 {    
-    ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);                 // Fast I/O for Competitive Programming
     cin.tie(NULL);
     ll int t;
-    cin>>t;
+    cin>>t;                                          // Taking input for test cases
     while(t--)
     {
         solve();
