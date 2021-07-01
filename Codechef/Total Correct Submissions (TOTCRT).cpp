16a7c85739e8883bdd0fd2a65f50addfa637a729
Manu Varghese <manuvarghese0603@gmail.com>
2021-07-01 14:39:39 +0530

    Added Intro and explantion

--- Codechef/Total Correct Submissions (TOTCRT).cpp
+++ Codechef/Total Correct Submissions (TOTCRT).cpp
@@ -1,24 +1,31 @@
+// Intro:
+// Problem link: https://www.codechef.com/problems/TOTCRT
+// In this question we have to create a hash map, map(string -> integer)
+// String (problem code) denotes the index and we have to check and insert the int value accordingly
+// Once all the values are inserted in the map, iterate through it and copy the integer value to a vector 
+// sort the vector and display it
+
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
 void solve()
 {
-    map<string,int> mp;
+    map<string,int> mp;             //defining map
     string s;
     int n,x;
     cin>>n;
     for(int i=0;i<3*n;i++)
     {
-        cin>>s>>x;
-        mp[s]+=x;
+        cin>>s>>x;                  // taking input of problem code and number of solutions
+        mp[s]+=x;                   // transfer the value to the map 
     }
     vector<int> ans;
     map<string,int>::iterator itr;
     for (itr = mp.begin(); itr != mp.end(); ++itr) 
-    ans.push_back(itr->second);
-    sort(ans.begin(),ans.end());
+    ans.push_back(itr->second);     //tranfer the int value to a vector
+    sort(ans.begin(),ans.end());    //sort the vector
     for(int i=0;i<ans.size();i++)
-    cout<<ans[i]<<" ";
+    cout<<ans[i]<<" ";              //output the vector
     cout<<"\n";
 }
 int main()
