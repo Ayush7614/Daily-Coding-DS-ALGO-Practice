fe8cffda9594dd45aa8d3362a0f83fac79c6e6a2
Manu Varghese <manuvarghese0603@gmail.com>
2021-07-01 14:30:38 +0530

    Added sample test cases

--- Codechef/Total Correct Submissions (TOTCRT).cpp
+++ Codechef/Total Correct Submissions (TOTCRT).cpp
@@ -33,3 +33,30 @@ int main()
     }
     return 0;
 }
+
+// Sample Input/Output
+
+
+// Example Input
+// 3
+// 1
+// A 1
+// B 2
+// C 3
+// 2
+// AA 1
+// AB 1
+// AB 1
+// AC 1
+// AC 1
+// AD 1
+// 1
+// Z 100
+// Z 100
+// Z 100
+
+
+// Example Output
+// 1 2 3
+// 1 1 2 2
+// 300
