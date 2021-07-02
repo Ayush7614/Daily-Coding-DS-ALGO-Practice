5c49768f041c7cb83b4001a77c59be8787cbadc1
Isha307 <56998032+Isha307@users.noreply.github.com>
2021-07-02 09:15:15 +0530

    Create Combination Sum.cpp

---
+++ Leetcode/Combination Sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void combinationSum(vector<int> &candidates, int target, vector<vector<int> > &res, vector<int> &combination, int begin) {
+        if (!target) {
+            res.push_back(combination);
+            return;
+        }
+        for (int i = begin; i != candidates.size() && target >= candidates[i]; ++i) {
+            combination.push_back(candidates[i]);
+            combinationSum(candidates, target - candidates[i], res, combination, i);
+            combination.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int> > res;
+        vector<int> combination;
+        combinationSum(candidates, target, res, combination, 0);
+        return res;
+    
+    }
+};
