925dfd09568fc47fd868a52cacd542eaa6ee7c2b
Vicky Gupta <vickygupta9987@gmail.com>
2021-07-02 14:13:53 +0530

    Solution for Sieve of Eratorthenes added in java

--- Gfg/SieveofEratorthenes.java
+++ Gfg/SieveofEratorthenes.java
@@ -1,3 +1,14 @@
+/*Given a number n, print all primes smaller than or equal to n. It is also given that n is a small number.
+
+Example:
+
+    Input : n =10
+    Output : 2 3 5 7
+
+    Input : n = 20
+    Output: 2 3 5 7 11 13 17 19
+*/
+//---------------------------------------------Solution------------------------------------------------------------
 package com.viki.session2;
 
 import java.util.Scanner;
