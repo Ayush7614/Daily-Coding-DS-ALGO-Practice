8f581297f52d4a4ecc4607a96a1dd87101faa9d9
sonam gupta <66966120+sonamgupta136@users.noreply.github.com>
2021-07-02 05:59:16 -0700

    Added Python Code
    Added #1628

---
+++ Data Structures/Merge K Sorted Linked List.py
@@ -0,0 +1,34 @@
+from Queue import PriorityQueue
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        head = point = ListNode(0)
+        q = PriorityQueue()
+        for l in lists:
+            if l:
+                q.put((l.val, l))
+        while not q.empty():
+            val, node = q.get()
+            point.next = ListNode(val)
+            point = point.next
+            node = node.next
+            if node:
+                q.put((node.val, node))
+        return head.next
+        
+
+
+#Your input
+#[[1,4,5],[1,3,4],[2,6]]
+#Output
+#[1,1,2,3,4,4,5,6]
+#Expected
+#[1,1,2,3,4,4,5,6]
