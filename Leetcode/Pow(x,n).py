542034bb135af80b6da1e4a36d592a300216d478
Rajesh <rajrk2024@gmail.com>
2021-07-02 12:42:58 +0530

    Added Pow(x,n) in Python

---
+++ Leetcode/Pow(x,n).py
@@ -0,0 +1,11 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return pow(x,n)
+
+#
+# Example test cases
+# Input: x = 2.00000, n = 10
+# Output: 1024.00000
+#
+# Input: x = 2.10000, n = 3
+# Output: 9.26100
