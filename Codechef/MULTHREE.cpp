acb8d25070a2ed83d47b9b5fba5035ab680fcde0
Manu Varghese <manuvarghese0603@gmail.com>
2021-07-02 13:09:56 +0530

    Added Intro and Explanation

--- Codechef/MULTHREE.cpp
+++ Codechef/MULTHREE.cpp
@@ -1,3 +1,24 @@
+// Intro:
+// Problem link: https://www.codechef.com/LRNDSA01/problems/MULTHREE
+
+// Construction of number:
+// 1st term = d0
+// 2nd term = d1
+// 3rd term(d2) = (d0+d1)%10
+// 4th term(d3) = (d0+d1+d2)%10 which can be written as (2(d0+d1))%10
+// 5th term(d4) = (3(d0+d1))%10
+// .
+// .
+// kth term (dk) = 2^(k-2)(d0+d1)%10
+
+// Condition of divisibility:
+// For a number to be divisible by 3 the sum of individual digit should be divisible by 3
+// i.e. (d0+d1+d2+d3+....+dk)%3 == 0
+
+// Since the constraints of the given problem is large therefore we cannot simply find the sum of all digit and then check if it is divisible by 3 or not
+// In this case every 4th value repeats itself(counting from the 4th term)
+// Therfore we have to find how many such cycles exist and then add the leftover digits
+
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
@@ -10,18 +31,18 @@ void solve()
     sum=s;
     else
     {
-        cvalue = ((2*s)%10) + ((4*s)%10) + ((8*s)%10) + ((6*s)%10);
-        cycles=(k-3)/4;
-        left = (k-3)%4;
-        temp=2;
+        cvalue = ((2*s)%10) + ((4*s)%10) + ((8*s)%10) + ((6*s)%10);         //calculating value of a single cycle 
+        cycles=(k-3)/4;                                                     //calculating number of cycles
+        left = (k-3)%4;                                                     //calculating leftover digits
+        temp=2;                                                                     
         while(left--)
         {
             lvalue = lvalue + (temp*s)%10;
-            temp = (temp*2)%10;
+            temp = (temp*2)%10;                                            //calculating the total of leftover digits
         }
-        sum = s + s%10 + cycles*cvalue + lvalue;
+        sum = s + s%10 + cycles*cvalue + lvalue;                           //Sum = first two digits + 3rd digit + no. of cycles*their frequency + leftover digits
     }
-    if(sum%3==0)
+    if(sum%3==0)                                                           // check if sum of all digits is divisible by 3 or not 
     cout<<"YES";
     else 
     cout<<"NO";
