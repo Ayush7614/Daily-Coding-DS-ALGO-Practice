97fc65d9a857730e1ca94c04e1050ffcff288a5b
Manu Varghese <manuvarghese0603@gmail.com>
2021-07-03 10:02:41 +0530

    Added solution of Wave problem - codechef

---
+++ Codechef/WAV2.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll int n,q,a,x,index,i;
+    cin>>n>>q;
+    ll int v[n];
+    for(i=0;i<n;i++)
+    cin>>v[i];
+    sort(v,v+n);    
+    while(q--)
+    {
+        cin>>x;
+        a=lower_bound(v,v+n,x)-v;
+        index = n-a;
+        if(v[a]==x)
+        cout<<"0";
+        else if(index%2==0  || x>v[n-1])
+        cout<<"POSITIVE";
+        else
+        cout<<"NEGATIVE";
+        cout<<"\n";
+    }
+    return 0;
+}
