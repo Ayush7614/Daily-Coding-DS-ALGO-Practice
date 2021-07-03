d11212b6413a4be7432e821b8ae845ccc5bc1977
NIDHI CHAURASIA <77978770+NidhiChaurasia@users.noreply.github.com>
2021-07-03 16:49:16 +0530

    Add files via upload

---
+++ Codechef/relativity.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int g,c;
+        cin>>g>>c;
+        long long int v=c*c;
+        long long int x=2*g;
+        cout<<v/x<<endl;
+    }
+	// your code goes here
+	return 0;
+}
