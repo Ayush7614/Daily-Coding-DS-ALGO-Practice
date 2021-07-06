95d4542fac5a76bf24be6e72fa48071099212d74
Rounak Chauhan <rounakchauhan70@gamil.com>
2021-07-07 01:02:47 +0530

    merge request for issue 1548

---
+++ Leetcode/Check If Two String Arrays are Equivalent.java
@@ -0,0 +1,24 @@
+class solution {
+    public boolean arrayStringsAreEqual() {
+        String word1[]= {"ab", "c"};
+        String word2[] = {"a", "bc"};
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        for (int i = 0; i < word1.length; i++) {
+            sb1.append(word1[i]);
+        }
+        for (int i = 0; i < word2.length; i++) {
+            sb2.append(word2[i]);
+        }
+        return sb1.toString().equals(sb2.toString()) ? true : false;
+    }
+}
+    
+
+/*Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
+Output: true
+Explanation:
+word1 represents string "ab" + "c" -> "abc"
+word2 represents string "a" + "bc" -> "abc"
+The strings are the same, so return true.*/
+
