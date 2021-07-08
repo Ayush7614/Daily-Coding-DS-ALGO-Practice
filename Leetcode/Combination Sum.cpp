0a1a0a7de4780fdbde77904eb4914d8e03c3d055
Isha307 <56998032+Isha307@users.noreply.github.com>
2021-07-08 10:49:01 +0530

    Update Combination Sum.cpp

--- Leetcode/Combination Sum.cpp
+++ Leetcode/Combination Sum.cpp
@@ -20,3 +20,45 @@ public:
     
     }
 };
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> A;
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            A.push_back(x);
+        }   
+        int sum;
+        cin>>sum;
+        Solution ob;
+        vector<vector<int>> result = ob.combinationSum(A, sum);
+   		if(result.size()==0){
+   			cout<<"Empty";
+   		}
+        for(int i=0;i<result.size();i++){
+            cout<<'(';
+            for(int j=0;j<result[i].size();j++){
+                cout<<result[i][j];
+                if(j<result[i].size()-1)
+                    cout<<" ";
+            }
+            cout<<")";
+        }
+        cout<<"\n";
+    }
+}
+/*
+candidates = [2,3,6,7], target = 7
+[[2,2,3],[7]]
+candidates = [1], target = 2
+[[1,1]]
+candidates = [2], target = 1
+[]
+candidates = [1], target = 1
+[[1]]
+*/
