e8458ddd445acea98858ffc364fe1b888c3d99f0
geetika1723 <geetikasethi1723@gmail.com>
2021-07-08 14:46:44 +0530

    Updated Binary Tree File

--- Leetcode/BinaryTree.java
+++ Leetcode/BinaryTree.java
@@ -1,4 +1,9 @@
 /**
+ * Given two integer arrays preorder and inorder where preorder is the preorder traversal of 
+ * a binary tree and inorder is the inorder traversal of the same tree, construct and return 
+ * the binary tree.
+ */
+/**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
@@ -13,9 +18,11 @@
  *     }
  * }
  */
-class Solution
+import java.util.*;
+class BinaryTree
 {
     int pr=0;
+    //Function to find index of value in inorder[]
     public int search(int inorder[],int x)
     {
         for(int i=0;i<inorder.length;i++)
@@ -23,22 +30,92 @@ class Solution
                 return i;
         return -1;
     }
+    //Function to build binary Tree
     public TreeNode buildTree(int[] preorder, int[] inorder)
     {
         if(inorder.length==0)
             return null;
+        //Pick current node from preorder traversal using pr(increament index)
         TreeNode tNode=new TreeNode(preorder[pr++]);
+        //If node has no children
         if(inorder.length==1)
             return tNode;
+        //find the index of this node in inorder traversal
         int id=search(inorder,tNode.val);
         int inL[]=new int[id];
         int inR[]=new int[inorder.length-1-id];int k=0;
+        // Storing left and right elements of node in different array
         for(int i=0;i<id;i++)
             inL[i]=inorder[i];
         for(int i=id+1;i<inorder.length;i++)
             inR[k++]=inorder[i];
+      //using index in inorder traversal ,construct left and right subtrees
         tNode.left=buildTree(preorder,inL);
         tNode.right=buildTree(preorder,inR);
         return tNode;
+    }
+    /* function to print level order traversal of tree*/
+    void printLevelOrder(TreeNode root)
+    {
+        int h = height(root);
+        int i;
+        for (i=1; i<=h; i++)
+            printCurrentLevel(root, i);
+    }
+ 
+    /* Compute the "height" of a tree -- the number of
+    nodes along the longest path from the root node
+    down to the farthest leaf node.*/
+    int height(TreeNode root)
+    {
+        if (root == null)
+           return 0;
+        else
+        {
+            /* compute  height of each subtree */
+            int lheight = height(root.left);
+            int rheight = height(root.right);
+             
+            /* use the larger one */
+            if (lheight > rheight)
+                return(lheight+1);
+            else return(rheight+1);
+        }
+    }
+ 
+    /* Print nodes at the current level */
+    void printCurrentLevel (TreeNode root ,int level)
+    {
+        if (root == null)
+        {
+        	System.out.print("null ");
+        	return;
+        }
+        if (level == 1)
+            System.out.print(root.val + " ");
+        else if (level > 1)
+        {
+            printCurrentLevel(root.left, level-1);
+            printCurrentLevel(root.right, level-1);
+        }
+    }
+     
+    public static void main(String args[])
+    {
+    	Scanner sc=new Scanner(System.in);
+    	Domino t=new Domino();
+    	System.out.print("Give no. of variables of binary tree");
+    	int n=sc.nextInt();
+    	int p[]= new int[n];
+    	int in[]= new int[n];
+    	System.out.println("Write preorder");
+    	for(int i=0;i<n;i++)
+    		p[i]=sc.nextInt();
+    	System.out.println("Write inorder");
+    	for(int i=0;i<n;i++)
+    		in[i]=sc.nextInt();
+    	TreeNode r=t.buildTree(p,in);
+    	System.out.println("Resultant Binary Tree");
+    	t.printLevelOrder(r);
     }
 }
