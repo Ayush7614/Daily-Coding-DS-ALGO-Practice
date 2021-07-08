57489e9c4647672fa094e1a0be25599e80c8a04a
Rajesh <rajrk2024@gmail.com>
2021-07-08 12:28:54 +0530

    Added Code for Input in Pow(x,n)

--- Leetcode/Pow(x,n).py
+++ Leetcode/Pow(x,n).py
@@ -1,11 +1,17 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
+def myPow( x, n) :
         return pow(x,n)
+
+x = float(input("Enter number : "))
+n = float(input("Enter power : "))
+print(myPow(x,n))
+
 
 #
 # Example test cases
-# Input: x = 2.00000, n = 10
+# Input: Enter number :  2.00000, Enter Power : 10
 # Output: 1024.00000
 #
-# Input: x = 2.10000, n = 3
+# Input: Enter number : 2.10000, Enter number : 3
 # Output: 9.26100
