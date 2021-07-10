d8a0c16b106d61bd3e3a7421ee0589e0172f0a64
Rajesh Kumar <80545227+rajeshkumar2024@users.noreply.github.com>
2021-07-10 23:49:36 +0530

    Update contains-duplicate.java

--- Leetcode/contains-duplicate.java
+++ Leetcode/contains-duplicate.java
@@ -1,5 +1,20 @@
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Size of array : ");
+        int n= sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0 ; i < n ; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(containsDuplicate(arr));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+
         Map<Integer,Integer> map = new HashMap<Integer,Integer>();
         for (int i = 0 ; i < nums.length ; i++){
             if(!map.containsKey(nums[i])){
@@ -13,9 +28,9 @@ class Solution {
     }
 }
 
+
+
 // Example test case
-// Input: nums = [1,2,3,1]
+// Input: Size = 4
+// 1 2 3 1
 // Output: true
-//
-// Input: nums = [1,2,3,4]
-// Output: false
