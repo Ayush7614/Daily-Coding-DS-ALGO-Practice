4c7ec93fbe69599f920a88c8b73d53fc07f3b064
GUNJAN GUPTA <81305824+gunjan-g@users.noreply.github.com>
2021-07-15 11:57:54 +0530

    Added comments

--- Hackerank/C++/Vector-Sort.cpp
+++ Hackerank/C++/Vector-Sort.cpp
@@ -10,17 +10,23 @@ using namespace std;
 
 
 int main() {
+    
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    
     int i,size,ele;
     vector<int> v;
+    
     cin>>size;
+    
     for(i=0;i<size;i++){
         cin>>ele;
-        v.push_back(ele);
+        v.push_back(ele);     //pushes the element in the array as they are entered by the user
     }
-    sort(v.begin(),v.end());
+    
+    sort(v.begin(),v.end());   //inbuilt function in STL which sorts the array from beginning to end (parameters)
+    
     for(i=0;i<size;i++){
-        cout<<v[i]<<" ";
+        cout<<v[i]<<" ";      //displaying the contents of the array
     }
      
     return 0;
