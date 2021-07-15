a90f56f171f040e76dc4bed6819e0dcbd6248de1
GUNJAN GUPTA <81305824+gunjan-g@users.noreply.github.com>
2021-07-16 02:30:37 +0530

    Added testcase

--- Hackerank/C++/Sets-STL.cpp
+++ Hackerank/C++/Sets-STL.cpp
@@ -1,5 +1,29 @@
 /* Problem Link:
-   https://www.hackerrank.com/challenges/cpp-sets/problem */
+   https://www.hackerrank.com/challenges/cpp-sets/problem 
+   
+Ques.
+You will be given Q queries. Each query is of one of the following three types:
+
+1 : Add an element x to the set.
+2 : Delete an element x from the set. (If the number x is not present in the set, then do nothing).
+3 : If the number x is present in the set, then print "Yes"(without quotes) else print "No"(without quotes).
+
+Sample Input:
+8
+1 9
+1 6
+1 10
+1 4
+3 6
+3 14
+2 6
+3 6
+
+Sample Output:
+Yes
+No
+No
+*/
 
 #include <cmath>
 #include <cstdio>
