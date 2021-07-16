dae2b81791efaafd8810156340d4a9d716cff2d3
JahnaviKashyap19 <kashyapjahnavi2018@gmail.com>
2021-07-17 01:20:44 +0530

    Added C program for union of two sorted array

--- Gfg/UnionOfTwoSortedArrays.c
+++ Gfg/UnionOfTwoSortedArrays.c
@@ -3,17 +3,11 @@
  Example:
  Input:
  arr1[] = { 1, 2, 4, 5, 8,9 }
- arr2[] = { 2, 3, 5, 7 }
- Output: Union : {1,2,3,4,5,7,8,9}  
+ arr2[] = { 2, 3, 5, 6,10 }
+ Output: Union : {1,2,3,4,5,6,8,9,10}  
 */
-
-
 #include <stdio.h>
- 
-/* Function prints union of arr1[] and arr2[]
-   m is the number of elements in arr1[]
-   n is the number of elements in arr2[] */
-void printUnion(int arr1[], int arr2[], int m, int n)
+ void UnionArray(int arr1[], int arr2[], int m, int n)
 {
     int i = 0, j = 0;
     while (i < m && j < n) {
@@ -34,14 +28,12 @@ void printUnion(int arr1[], int arr2[], int m, int n)
         printf(" %d ", arr2[j++]);
 }
  
-/* Driver program to test above function */
 int main()
 {
     int arr1[] = { 1, 2, 4, 5, 8,9 };
-    int arr2[] = { 2, 3, 5, 7 };
-    int m = sizeof(arr1) / sizeof(arr1[0]);
-    int n = sizeof(arr2) / sizeof(arr2[0]);
-    printUnion(arr1, arr2, m, n);
-    getchar();
+    int arr2[] = { 2, 3, 5, 6,10 };
+    int size1 = sizeof(arr1) / sizeof(arr1[0]);
+    int size2 = sizeof(arr2) / sizeof(arr2[0]);
+    UnionArray(arr1, arr2, size1, size2);
     return 0;
 }
