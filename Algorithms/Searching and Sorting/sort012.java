7c9bbb299354d3b2264421eb8d9c75f353259052
Rounak Chauhan <77567098+Rounak-chauhan@users.noreply.github.com>
2021-07-16 16:25:12 +0530

    Update sort012.java

--- Algorithms/Searching and Sorting/sort012.java
+++ Algorithms/Searching and Sorting/sort012.java
@@ -1,47 +1,59 @@
-import java.util.Scanner;
-public class sort012 {
-    public class sorting
-     {
-         public void sort()
-         {
-        Scanner sc=new Scanner(System.in);
-                System.out.println("enter the size of the array which just conist of 0,1,2");
-                int n= sc.nextInt();
-                int []arr=new int[n];
-                for(int l=0;l<n;l++)
-                {
-                    arr[l]=sc.nextInt();
-                }
-    
-                // code here
-                int i = 0;
-                int j = 0;
-                int k = arr.length - 1;
-                while (i <= k)
-                {
-                    if (arr[i] == 0) {
-                        int temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
-                        i++;
-                        j++;
-    
-                    } else if (arr[i] == 1) {
-                        i++;
-                    } else {
-                        int temp = arr[i];
-                        arr[i] = arr[k];
-                        arr[k] = temp;
-                        k--;
-                    }
-                }
-                for(int r=0;r<n;r++)
-                {
-                    System.out.println(arr[r]);
-                }
-            }
-        }
-    
-        
-    }
-   
+// Java program to sort an array of 0, 1 and 2
+import java.io.*;
+
+class countzot {
+	static void sort012(int a[], int arr_size)
+	{
+		int lo = 0;
+		int hi = arr_size - 1;
+		int mid = 0, temp = 0;
+		while (mid <= hi) {
+			switch (a[mid]) {
+			case 0: {
+				temp = a[lo];
+				a[lo] = a[mid];
+				a[mid] = temp;
+				lo++;
+				mid++;
+				break;
+			}
+			case 1:
+				mid++;
+				break;
+			case 2: {
+				temp = a[mid];
+				a[mid] = a[hi];
+				a[hi] = temp;
+				hi--;
+				break;
+			}
+			}
+		}
+	}
+
+	/* Utility function to print array arr[] */
+	static void printArray(int arr[], int arr_size)
+	{
+		int i;
+		for (i = 0; i < arr_size; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println("");
+	}
+
+	/*Driver function to check for above functions*/
+	public static void main(String[] args)
+	{
+		int arr[] = { 0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 };
+		int arr_size = arr.length;
+		sort012(arr, arr_size);
+		System.out.println("Array after seggregation ");
+		printArray(arr, arr_size);
+	}
+}
+/*test cases
+Input: {0, 1, 2, 0, 1, 2}
+Output: {0, 0, 1, 1, 2, 2}
+
+Input: {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1}
+Output: {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2}
+*/
