2741af3031352069bfcabdce0c510cf9f32dd371
surbhi594 <surbhisri64@gmail.com>
2021-07-16 19:02:38 +0530

    Resolved conflicts

--- Leetcode/oddString.cpp
+++ Leetcode/oddString.cpp
@@ -1,4 +1,4 @@
- /*link of this question : https://leetcode.com/problems/largest-odd-number-in-string/
+/*link of this question : https://leetcode.com/problems/largest-odd-number-in-string/
 
 
   You are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
