f33ff86b2a818922c964b9a48826fa2a794cf7e8
SrihariTadala <60476477+SrihariTadala@users.noreply.github.com>
2021-07-16 13:35:24 +0530

    Add files via upload

---
+++ Leetcode/Java/power.java
@@ -0,0 +1,27 @@
+class Solution {
+    public double myPow(double x, int n) {
+        
+        double ans;
+        if(x <= 0.00001 && x>0)
+            return 0.0;
+        if(x == 1)
+            return 1.0;
+        if(x == -1)
+        {
+            return (n%2 == 0) ? 1.0 : -1.0;
+        }
+        if(n < -100 )
+            return 0.0;
+        ans = (n >= 0 )?positive(x,n) : (double)1/positive(x,n*-1);
+        return ans;
+    }
+    double positive(double x,int n)
+    {
+        if(n == 1)
+            return x;
+        if(n == 0)
+            return 1.0;
+        return x*positive(x,n-1);
+    }
+    
+}
