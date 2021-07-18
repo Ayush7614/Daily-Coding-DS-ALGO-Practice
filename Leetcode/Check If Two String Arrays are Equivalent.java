9fc2778c8d699223e7b96a8041656d48954de145
Rounak Chauhan <77567098+Rounak-chauhan@users.noreply.github.com>
2021-07-18 17:53:45 +0530

    Update Check If Two String Arrays are Equivalent.java

--- Leetcode/Check If Two String Arrays are Equivalent.java
+++ Leetcode/Check If Two String Arrays are Equivalent.java
@@ -1,19 +1,30 @@
-class solution {
-    public boolean arrayStringsAreEqual() {
-        String word1[]= {"ab", "c"};
-        String word2[] = {"a", "bc"};
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-        for (int i = 0; i < word1.length; i++) {
-            sb1.append(word1[i]);
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Main
+{
+  public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        int i1 = 0, j1 = 0, i2 = 0, j2 = 0;
+        while(true){
+            if(word1[i1].charAt(j1) != word2[i2].charAt(j2)) return false;
+            if(j1 == word1[i1].length()-1){i1++; j1 = 0;}
+            else j1++;
+            if(j2 == word2[i2].length()-1){i2++; j2 = 0;}
+            else j2++;
+            if(i1 == word1.length && i2 == word2.length)
+                return true;
+            else if(i1 == word1.length || i2 == word2.length)
+                return false;
         }
-        for (int i = 0; i < word2.length; i++) {
-            sb2.append(word2[i]);
-        }
-        return sb1.toString().equals(sb2.toString()) ? true : false;
     }
+  public static void main (String[] args) throws java.lang.Exception
+  {
+    String[] word1 = {"ab", "c"};
+    String[] word2 = {"a", "bc"};
+    System.out.print((arrayStringsAreEqual(word1, word2) ? "true" : "false"));
+    return 0;
+  }
 }
-    
 
 /*Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
 Output: true
