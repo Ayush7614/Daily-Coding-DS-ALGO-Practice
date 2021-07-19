a90b088c30398760e3a5bd4e84e9e262ea2c8043
Shivam Singh <76156804+2000shivam659@users.noreply.github.com>
2021-07-19 22:38:11 +0530

    Updated Maximum Length of Repeated Subarray.java

--- Leetcode/Java/Maximum Length of Repeated Subarray.java
+++ Leetcode/Java/Maximum Length of Repeated Subarray.java
@@ -17,3 +17,13 @@ class Solution {
     return max;
     }
 }
+
+Example 1:
+
+Input: nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
+Output: 3
+Explanation: The repeated subarray with maximum length is [3,2,1].
+Example 2:
+
+Input: nums1 = [0,0,0,0,0], nums2 = [0,0,0,0,0]
+Output: 5
