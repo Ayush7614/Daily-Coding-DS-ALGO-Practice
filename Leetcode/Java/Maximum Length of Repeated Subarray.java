f1cd9cc24e5502948d31802d4b0c343cc40f595a
Shivam Singh <76156804+2000shivam659@users.noreply.github.com>
2021-07-20 21:39:12 +0530

    new-solution "Maximum Length of Repeated Subarray"

--- Leetcode/Java/Maximum Length of Repeated Subarray.java
+++ Leetcode/Java/Maximum Length of Repeated Subarray.java
@@ -1,8 +1,9 @@
 // Question  https://leetcode.com/problems/maximum-length-of-repeated-subarray/
 
-// Solution
+import java.util.Scanner;
+
 class Solution {
-    public int findLength(int[] A, int[] B) {
+    public static int findLength(int[] A, int[] B) {
         int dp[][] = new int[A.length+1][B.length+1];
         int max = 0;
         for(int i = 0; i <= A.length; i++) {
@@ -14,16 +15,32 @@ class Solution {
                 }
             }
         }
-    return max;
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int alen = sc.nextInt();
+        int blen = sc.nextInt();
+
+        int[] A = new int[alen];
+        for(int i = 0;i < alen;i++)
+            A[i] = sc.nextInt();
+
+        int[] B = new int[blen];
+        for(int i = 0;i < alen;i++)
+            B[i] = sc.nextInt();
+
+        System.out.println( findLength(A, B) );
     }
 }
 
-Example 1:
-
-Input: nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
-Output: 3
-Explanation: The repeated subarray with maximum length is [3,2,1].
-Example 2:
-
-Input: nums1 = [0,0,0,0,0], nums2 = [0,0,0,0,0]
-Output: 5
+//Example 1:
+//        Input: nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
+//        Output: 3
+//        Explanation: The repeated subarray with maximum length is [3,2,1].
+//
+// Example 2:
+//        Input: nums1 = [0,0,0,0,0], nums2 = [0,0,0,0,0]
+//        Output: 5
