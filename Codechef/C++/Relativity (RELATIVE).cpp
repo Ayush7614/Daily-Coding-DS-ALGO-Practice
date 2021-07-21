f4fd4b98c7e825074164953cf23c3192411f83a1
Manu Varghese <manuvarghese0603@gmail.com>
2021-07-21 12:26:31 +0530

    Added solution code

---
+++ Codechef/C++/Relativity (RELATIVE).cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+void solve()
+{
+    int g,c;
+    cin>>g>>c;
+    cout<<(c*c)/(2*g)<<"\n";
+}
+int main()
+{    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
