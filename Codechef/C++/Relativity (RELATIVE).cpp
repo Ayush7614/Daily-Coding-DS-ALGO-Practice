7235470026d6f9b159721147699ee4ceb7d14c08
Manu Varghese <manuvarghese0603@gmail.com>
2021-07-21 12:33:24 +0530

    Added introduction and explanation

--- Codechef/C++/Relativity (RELATIVE).cpp
+++ Codechef/C++/Relativity (RELATIVE).cpp
@@ -1,3 +1,10 @@
+// Intro:
+// Problem link: https://www.codechef.com/JULY21C/problems/RELATIVE
+// Explanation:
+// We have to find the height from the formula v^2 = 2*g*h
+// Here we have to find the height and it given that the velocity is that of speed of light therefore the formula can be further modified as c^2/2*g = h 
+// The speed of light c and acceleration due to gravity is taken as input, so we have to apply the above formula and output thee height in meters
+
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
