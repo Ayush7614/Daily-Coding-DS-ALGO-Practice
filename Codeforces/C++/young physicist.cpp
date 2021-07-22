128199c51057a40b9879545707bc03c1fedd8b67
MehaShukla <meha1212shukla@gmail.com>
2021-07-22 19:26:38 +0530

    NEW solution

---
+++ Codeforces/C++/young physicist.cpp
@@ -0,0 +1,28 @@
+// https://codeforces.com/problemset/problem/69/A
+#include <iostream>
+using namespace std;
+#include <cmath>
+#include <algorithm>
+int main()
+{
+    int t;
+    cin>>t;
+    int s1=0,s2=0,s3=0,v=0;
+while(t--)
+{
+      
+      int a,b,c;
+      
+      cin>>a>>b>>c;
+      s1+=a;
+      s2+=b;
+      s3+=c;
+   
+      
+}
+      if(s1==0&&s2==0&&s3==0)
+      cout<<"YES"<<'\n';
+      else
+      cout<<"NO"<<'\n';
+      return 0;
+  }
