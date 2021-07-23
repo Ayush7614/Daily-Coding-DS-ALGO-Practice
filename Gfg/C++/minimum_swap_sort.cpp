a7cf7cb2ed5eeaee17c68e218ff5672023db5d59
Baljeet Singh <prince.singh4000@gmail.com>
2021-07-23 22:22:20 +0530

    Update minimum_swap_sort.cpp

--- Gfg/C++/minimum_swap_sort.cpp
+++ Gfg/C++/minimum_swap_sort.cpp
@@ -1,3 +1,12 @@
+/*Given an array of n distinct elements. Find the minimum number of swaps required to sort the array in strictly increasing order.
+
+Input:
+nums = {10, 19, 6, 3, 5}
+Output:
+2
+Explaination:
+swap 10 with 3 and swap 19 with 5.
+*/
 
 #include<bits/stdc++.h>
 using namespace std;
