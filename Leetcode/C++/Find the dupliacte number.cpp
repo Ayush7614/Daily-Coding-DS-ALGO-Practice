8408ad5681a4d0438908411f924dd3ca9c57534e
Sneha Agarwal <73896596+SnehaAgarwal361@users.noreply.github.com>
2021-07-23 18:38:51 +0530

    Update Find the dupliacte number.cpp

--- Leetcode/C++/Find the dupliacte number.cpp
+++ Leetcode/C++/Find the dupliacte number.cpp
@@ -12,23 +12,37 @@ Input: nums = [1,3,4,2,2]
 Output: 2
 
 */
-
-class Solution {
-public:
+#include<iostream>
+using namespace std;
     int findDuplicate(vector<int>& nums) {
         int t,h;
-        t=nums[0];
-        h=nums[0];
+        t=nums[0];//storing 0th index
+        h=nums[0];//storing 0th index
         do{
             t=nums[t];
             h=nums[nums[h]];
         }while(h!=t);
         t=nums[0];
-        while(h!=t)
+        while(h!=t)//when h==t we will get the sol
         {
             t=nums[t];
             h=nums[h];
         }
         return h;
+    }  
+// Driver Code
+int main()
+{
+    int arr[] = { 1, 2, 3, 1 };
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+    vector<int> nums;
+    for(int i=0;i<arr_size;i++)
+    {
+        nums.push_back(arr[i]);
     }
-};
+    int ans=findDuplicate( nums);
+    cout<<ans;
+   return 0;
+}
+ 
+    
