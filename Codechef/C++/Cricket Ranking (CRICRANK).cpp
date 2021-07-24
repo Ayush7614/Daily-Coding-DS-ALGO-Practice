8613ff2f47ad250638847be5447b6756c20277fc
Manu Varghese <manuvarghese0603@gmail.com>
2021-07-24 21:58:52 +0530

    Added Explantion

--- Codechef/C++/Cricket Ranking (CRICRANK).cpp
+++ Codechef/C++/Cricket Ranking (CRICRANK).cpp
@@ -1,12 +1,17 @@
+// Problem link: https://www.codechef.com/START6C/problems/CRICRANK
+// We have to maintain a counter and increase it if r1>r2,w1>w2 or c1>c2 condition arises
+// We have to output A if player A is better than B, A is better than B if more than 1 of the above conditions are satisfied, i.e count>1
+// If count value is <=1 it implies that B is better therefore output B
+
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
 void solve()
 {
     int a[3],b[3],cnt=0;
-    for(int i=0;i<3;i++)
+    for(int i=0;i<3;i++)        // array is used to take inputs instead of r1,w1,c1 to make comparison easy 
     cin>>a[i];
-    for(int i=0;i<3;i++)
+    for(int i=0;i<3;i++)        // array is used to take inputs instead of r2,w2,c2
     cin>>b[i];
     for(int i=0;i<3;i++)
     {
