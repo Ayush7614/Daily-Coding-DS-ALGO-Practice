10c1c8bf89c6fc767d0e6fabd6edd8f0eba5fd40
SrihariTadala <60476477+SrihariTadala@users.noreply.github.com>
2021-07-25 15:38:51 +0530

    updated

--- Algorithms/Backtracking/Java/permut.java
+++ Algorithms/Backtracking/Java/permut.java
@@ -1,58 +1,42 @@
-import java.util.Scanner;
-
-public class Permutation {
-    
-    
-    
-    public static void main (String args[])
-    {
-        System.out.println("Please enter the string whose permutations we need to show ");
-        Scanner in = new Scanner(System.in);
-        String original=in.nextLine();
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("Results are :");
-        System.out.println("");
-        System.out.println("");
-        permute(original);
-    }
-    
-    
-    
-    public static   void permute( String input)
-    {
-        int inputLength = input.length();
-        boolean[ ] used = new boolean[ inputLength ];
-        StringBuffer outputString = new StringBuffer();
-        char[ ] in = input.toCharArray( );
-        doPermute ( in, outputString, used, inputLength, 0 );
+public class PermuteString {    
+    //Function for swapping the characters at position I with character at position j    
+    public static String swapString(String a, int i, int j) {    
+        char[] b =a.toCharArray();    
+        char ch;    
+        ch = b[i];    
+        b[i] = b[j];    
+        b[j] = ch;    
+        return String.valueOf(b);    
+    }    
         
-    }
-    
-    public static    void doPermute ( char[ ] in, StringBuffer outputString,
-    boolean[ ] used, int inputLength, int level)
-    {
-        if( level == inputLength) {
-            System.out.println ( outputString.toString());
-            return;
-        }
+    public static void main(String[] args)    
+    {    
+        String str = "ABC";    
+        int len = str.length();    
+        System.out.println("All the permutations of the string are: ");    
+        generatePermutation(str, 0, len);    
+    }    
         
-        for( int i = 0; i < inputLength; ++i )
-        {
-            
-            if( used[i] ) continue;
-            
-            outputString.append( in[i] );
-            used[i] = true;
-            doPermute( in,   outputString, used, inputLength, level + 1 );
-            used[i] = false;
-            outputString.setLength(   outputString.length() - 1 );
-        }
-    }
-    
-}
-
+    //Function for generating different permutations of the string    
+    public static void generatePermutation(String str, int start, int end)    
+    {    
+        //Prints the permutations    
+        if (start == end-1)    
+            System.out.println(str);    
+        else    
+        {    
+            for (int i = start; i < end; i++)    
+            {    
+                //Swapping the string by fixing a character    
+                str = swapString(str,start,i);    
+                //Recursively calling function generatePermutation() for rest of the characters     
+                generatePermutation(str,start+1,end);    
+                //Backtracking and swapping the characters again.    
+                str = swapString(str,start,i);    
+            }    
+        }    
+    }    
+}    
 
 ///
 input
