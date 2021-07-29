8ccbbbeb8cdf8b6702a28d1ca79b5f970af8d808
Manu Varghese <manuvarghese0603@gmail.com>
2021-07-29 23:45:16 +0530

    Added solution code

---
+++ Codechef/C++/Ceiling Sum (CEILSUM).cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+void solve()
+{
+    ll int a,b;
+    cin>>a>>b;
+    if(a==b)
+    cout<<"0";
+    else
+    {
+        if((b-a)%2 == 0)
+        cout<<(b-a+2)/2;
+        else
+        cout<<(b-a+1)/2;
+    }
+    cout<<"\n";
+}
+int main()
+{    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
