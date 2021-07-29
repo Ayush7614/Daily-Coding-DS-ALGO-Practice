d806213cefde8138c43cb62690ce0fba36a62c9e
Manu Varghese <manuvarghese0603@gmail.com>
2021-07-29 23:58:59 +0530

    Added Explanation

--- Codechef/C++/Ceiling Sum (CEILSUM).cpp
+++ Codechef/C++/Ceiling Sum (CEILSUM).cpp
@@ -1,3 +1,14 @@
+// Problem link: https://www.codechef.com/START7C/problems/CEILSUM
+// If  A==B then the only value of X possible wiil be A(or B) therefore the value of Ceil((B-B)/2)+Ceil((A-A)/2) = 0
+// Further there are 4 more conditions 
+// ceil(B-X)      ceil(X-A)     Max
+//    Even          Even        B-A/2 (since both are even therefore ceil dosen't matter)
+//    Even          Odd         (B-X + X-A + 1)/2 = (B-A+1)/2 
+//    Odd           Even        (B-X+1 + X-A)/2 = (B-A+1)/2
+//    Odd           Odd         (B-X+1 + X-A + 1)/2 = (B-A+2)/2
+// Therefore is both of them are even/odd the condition to find max value is (B-A+2)/2 (sinve we have to maximize)
+// If both different then the maximum will be  (B-A+1)/2
+
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
