fcd14d926bb3b359a81ccd9082c095e051ee0979
gunjan-g <gunjang1202@gmail.com>
2021-07-31 20:24:12 +0530

    adding testcases

--- Hackerank/C++/Designer PDF Viewer.cpp
+++ Hackerank/C++/Designer PDF Viewer.cpp
@@ -6,6 +6,19 @@ Ques.
 When a contiguous block of text is selected in a PDF viewer, the selection is highlighted with a blue rectangle. In this PDF viewer, each word is highlighted independently.
 There is a list of 26 character heights aligned by index to their letters. For example, 'a' is at index 0 and 'z' is at index 25. There will also be a string. Using the letter heights given, determine the area of the rectangle highlight in mm^2 assuming all letters are 1mm wide.
 
+Sample Input:
+1 3 1 3 1 4 1 3 2 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+abc
+
+Sample Output 0:
+9
+
+Explanation 0:
+We are highlighting the word abc:
+
+Letter heights are a=1, b=3 and c=1. The tallest letter, b, is 3mm high. The selection area for this word is 3.1.3mm= 9mm^3.
+Note: Recall that the width of each character is 1mm.
+
 */
 
 #include <bits/stdc++.h>
