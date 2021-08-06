3c49fbc2c02a64c1e740d1c0f2aa1315bf9452fa
Anshika Srivastava <67851072+Anshika-Srivastava@users.noreply.github.com>
2021-08-06 10:19:29 +0530

    Update Delete_without_head_pointer.cpp

--- Gfg/C++/Delete_without_head_pointer.cpp
+++ Gfg/C++/Delete_without_head_pointer.cpp
@@ -1,3 +1,5 @@
+//You are given a pointer/ reference to the node which is to be deleted from the linked list of N nodes. The task is to delete the node. Pointer/ reference to head node is not given.
+//Note: No head reference is given to you. It is guaranteed that the node to be deleted is not a tail node in the linked list.
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -115,3 +117,28 @@ int main(void)
 
 
   // } Driver Code Ends
+
+//Test Cases:
+/*
+Input:
+N = 2
+value[] = {1,2}
+node = 1
+Output: 2
+_____________________________________
+
+Input:
+N = 4
+value[] = {10,20,4,30}
+node = 20
+Output: 10 4 30
+
+*/
+
+//Time complexity
+/*
+For deleting node: O(N)
+
+Auxiliary Space: O(1)
+
+*/
