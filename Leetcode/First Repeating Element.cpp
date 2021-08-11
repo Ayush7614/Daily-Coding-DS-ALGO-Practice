a70764ad371b2ddc9aecff318d7a8c08d650cbae
Prashant <prashantchauhan3110@gmail.com>
2021-08-11 18:26:26 +0530

    First Repeating element program added

--- Leetcode/First Repeating Element.cpp
+++ Leetcode/First Repeating Element.cpp
@@ -18,3 +18,44 @@ Output :
 
 5 is appearing twice and its first appearance is at index "1" which is less than 3 whose first occuring index is "2".
 */
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter n :";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter elements :";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    
+    const int N=1e6+2;
+    int idx[N];
+    for(int i=0;i<N;i++)
+    {
+        idx[i]=-1;
+    }
+    
+    int minidx=INT_MAX;
+    for(int i=0;i<n;i++)
+    {
+        if(idx[arr[i]]!=-1)
+        {
+            minidx=min(minidx,idx[arr[i]]);
+        }
+        else
+        {
+            idx[arr[i]]=i;
+        }
+    }
+    
+    minidx==INT_MAX ? cout<<"-1" : cout<<minidx;
+    return 0;
+}
