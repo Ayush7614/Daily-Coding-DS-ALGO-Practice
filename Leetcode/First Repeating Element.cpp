670fe11a1012ffe470796ed013d015a1c43921c6
Prashant <prashantchauhan3110@gmail.com>
2021-08-11 18:24:02 +0530

    Initial commit

---
+++ Leetcode/First Repeating Element.cpp
@@ -0,0 +1,20 @@
+/* 
+Given an array arr[] of size n.
+The task is to find repeating element in the array of integers, i.e an element that occurs more than once and whose index of first occurence is smallest.
+
+constraints :
+
+1<=n<=10^6
+0<=Ai<=10^6
+
+Input :
+7
+
+1 5 3 4 3 5 6
+
+Output :
+1
+
+
+5 is appearing twice and its first appearance is at index "1" which is less than 3 whose first occuring index is "2".
+*/
