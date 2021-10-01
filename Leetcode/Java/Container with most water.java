9874b7dbabc25388fc49e347543eea7cde99236e
esheetaparulekar <esheeta.parulekar@cumminscollege.in>
2021-10-01 11:26:16 +0530

    Update Container with most water.java

--- Leetcode/Java/Container with most water.java
+++ Leetcode/Java/Container with most water.java
@@ -13,11 +13,11 @@ Output: 16
 
 class Solution {
     // function to calculate container with most water
-    public int maxArea(int[] height) {
+    public int maxArea(int[] height) {//array with heights of barriers is provided
         int maxwater = 0, left = 0, right = height.length - 1;
         while (left < right) {
-            maxwater = Math.max(maxwater, Math.min(height[left], height[right]) * (right - left));
-            if (height[left] < height[right])
+            maxwater = Math.max(maxwater, Math.min(height[left], height[right]) * (right - left));//updating maximum water stored
+            if (height[left] < height[right])//updating the left and right barrier values to store maximum water
                 left++;
             else
                 right--;
