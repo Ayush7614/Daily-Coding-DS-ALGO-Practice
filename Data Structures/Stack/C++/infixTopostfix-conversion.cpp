eaea3d9c5712a6653e1dc0a58e521d3f1fbee2d5
Ankit Srivastava <thisisankitsrivastava@gmail.com>
2021-10-09 23:18:59 +0530

    Convert Infix Expression to Postfix in C++
    Signed-off-by: Ankit Srivastava <thisisankitsrivastava@gmail.com>

---
+++ Data Structures/Stack/C++/infixTopostfix-conversion.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <iostream>
+#include <ctype.h>
+
+using namespace std;
+
+char stack[100];
+int top = -1;
+
+void push(char x)
+{
+    stack[++top] = x;
+}
+
+char pop()
+{
+    if (top == -1)
+        return -1;
+    else
+        return stack[top--];
+}
+
+int priority(char x)
+{
+    if (x == '(')
+        return 0;
+    if (x == '+' || x == '-')
+        return 1;
+    if (x == '*' || x == '/')
+        return 2;
+    return 0;
+}
+
+int main()
+{
+    char exp[100];
+    char *e, x;
+    cout << "Enter the expression : ";
+    cin >> exp;
+    cout << endl;
+    e = exp;
+
+    while (*e != '\0')
+    {
+        if (isalnum(*e))
+            cout << *e << " ";
+        else if (*e == '(')
+            push(*e);
+        else if (*e == ')')
+        {
+            while ((x = pop()) != '(')
+                printf("%c ", x);
+        }
+        else
+        {
+            while (priority(stack[top]) >= priority(*e))
+                cout << pop() << " ";
+            push(*e);
+        }
+        e++;
+    }
+
+    while (top != -1)
+    {
+        cout << pop() << " ";
+    }
+    return 0;
+}
